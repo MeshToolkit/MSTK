f1c9c660f7bad4b38cd9d6faa2fd6d14e21d91f5
rao <devnull@localhost>
2004-05-25 15:36:48 -0600

    Modified the printed out string

--- src/misc/List_PrintID.c
+++ src/misc/List_PrintID.c
@@ -10,7 +10,7 @@
 
     num = List_Num_Entries(l);
 
-    fprintf(stderr,"Set 0x%x:\n",l);
+    fprintf(stderr,"Set 0x%x:\n",(unsigned int) l);
     for (i = 0, k = 0; i < num; i++) {
       entry = List_Entry(l,i);
 
