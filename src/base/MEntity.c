979acf81a9455a18f642ab777424383470fc93fa
rao <devnull@localhost>
2004-05-25 15:33:27 -0600

    Added functions for attaching attribute instances or values to entities

--- src/base/MEntity.c
+++ src/base/MEntity.c
@@ -6,6 +6,7 @@
 #include "MEntity.h"
 #include "List.h"
 #include "MSTK_malloc.h"
+#include "MAttrib.h"
 #include "MSTK.h"
 
 #ifdef __cplusplus
@@ -60,10 +61,20 @@ extern "C" {
   }
 
   void MEnt_Mark(MEntity_ptr ent, int markerID) {
+#ifdef DEBUG
+    if (ent->dim < MVERTEX && ent->dim > MREGION)
+      MSTK_Report("MEnt_Unmark","Not a valid topological entity",ERROR);
+#endif
+
     ent->marker = ent->marker | 1<<(markerID-1);
   }
 
   void MEnt_Unmark(MEntity_ptr ent, int markerID) {
+#ifdef DEBUG
+    if (ent->dim < MVERTEX && ent->dim > MREGION)
+      MSTK_Report("MEnt_Unmark","Not a valid topological entity",ERROR);
+#endif
+
     ent->marker = ent->marker & ~(1<<(markerID-1));
   }
 
@@ -87,6 +98,92 @@ extern "C" {
     }
   }
 
+
+  /* Set value of attribute */
+  void MEnt_Set_AttVal(MEntity_ptr ent, MAttrib_ptr attrib, int ival, double lval, void *pval) {
+    int idx, found;
+    MAttIns_ptr attins;
+    List_ptr attinslist;
+    
+    if (!ent->AttInsList)
+      ent->AttInsList = List_New(3);
+      
+    attinslist = ent->AttInsList;
+    idx = 0; found = 0;
+    while ((attins = List_Next_Entry(attinslist,&idx))) {
+      if (MAttIns_Attrib(attins) == attrib) {
+	found = 1;
+	break;
+      }
+    }
+
+    if (!found) {
+      attins = MAttIns_New(attrib);
+      List_Add(attinslist,attins);
+    }
+ 
+    MAttIns_Set_Value(attins, ival, lval, pval);
+  }
+
+  /* Clear value of attribute */
+  void MEnt_Rem_AttVal(MEntity_ptr ent, MAttrib_ptr attrib) {
+    int i, idx, found;
+    MAttIns_ptr attins;
+    List_ptr attinslist;
+    
+    if (!ent->AttInsList)
+      return;
+    
+    attinslist = ent->AttInsList;
+    idx = 0; i = 0; found = 0;
+    while ((attins = List_Next_Entry(attinslist,&idx))) {
+      if (MAttIns_Attrib(attins) == attrib) {
+	found = 1;
+	break;
+      }
+      else
+	i++;
+    }
+
+    if (!found)
+      return;
+
+    List_Remi(attinslist,i);
+    MAttIns_Delete(attins);
+  }
+
+
+  /* Query the value of the attribute */
+  int MEnt_Get_AttVal(MEntity_ptr ent, MAttrib_ptr attrib, int *ival, double *lval, void **pval) {
+    int idx, found;
+    MAttIns_ptr attins;
+    List_ptr attinslist;
+    
+    if (ival) *ival = 0;
+    if (lval) *lval = 0;
+    if (pval) *pval = NULL;
+    
+    if (!ent->AttInsList)
+      return 0;
+      
+    attinslist = ent->AttInsList;
+    idx = 0; found = 0;
+    while ((attins = List_Next_Entry(attinslist,&idx))) {
+      if (MAttIns_Attrib(attins) == attrib) {
+	found = 1;
+	break;
+      }
+    }
+
+    if (!found)
+      return 0;
+ 
+    MAttIns_Get_Value(attins, ival, lval, pval);
+
+    return 1;
+  }
+  
+    
 
 #ifdef __cplusplus
 }
