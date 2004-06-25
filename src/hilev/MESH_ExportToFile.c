cca45baca65496b0cd86e6d2d7372411102ff352
rao <devnull@localhost>
2004-06-25 16:16:33 -0600

    Export mesh to an external file format

---
+++ src/hilev/MESH_ExportToFile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <MSTK.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int MESH_ExportToFile(Mesh_ptr mesh, const char *filename, const char *format,
+		      const int natt, const char **attnames) {
+
+  if (strncmp(format,"gmv",3) == 0) {
+    return MESH_ExportToGMV(mesh,filename,natt,attnames);
+  }
+  /*  else (strncmp(format,"avs",3) == 0) {
+    return MESH_ImportFromUCD(mesh,filename,natt,attnames);
+    } */
+  /*  else (strncmp(format,"dx",2) == 0) {
+    return MESH_ExportToDX(mesh,filename,natt,attnames);
+    } */
+  else {
+    MSTK_Report("MESH_ImportFromFile","Unsupported import format",ERROR);
+    return 0;
+  }
+
+}
+
+#ifdef __cplusplus
+}
+#endif
