68cf63cd7e768c1b3b3fbd9183c00caabc675718
rao <devnull@localhost>
2004-06-25 16:18:06 -0600

    Import mesh from an external file format

---
+++ src/hilev/MESH_ImportFromFile.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <MSTK.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int MESH_ImportFromFile(Mesh_ptr mesh, const char *filename, const char *format) {
+
+  if (strncmp(format,"gmv",3) == 0) {
+    return MESH_ImportFromGMV(mesh,filename);
+  }
+  /*  else (strncmp(format,"avs",3) == 0) {
+    return MESH_ImportFromUCD(mesh,filename);
+    } */
+  else {
+    MSTK_Report("MESH_ImportFromFile","Unsupported import format",ERROR);
+    return 0;
+  }
+
+}
+
+#ifdef __cplusplus
+}
+#endif
