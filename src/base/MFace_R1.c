be4c867f133c08a5093217b4ee592c95c4a95998
rao <devnull@localhost>
2004-11-05 16:19:09 -0700

    Introduced compact data storage for common entity data and operators for accessing and modifying that data
    Also number of upward or downward adjacencies is not stored anymore
    and is obtained from the list length instead
    
    Also Replaced some **_R1R2 and **_R3R4 functions with **_RN functions

--- src/base/MFace_R1.c
+++ src/base/MFace_R1.c
@@ -10,99 +10,150 @@ extern "C" {
 #endif
 
   void MF_Set_RepType_R1(MFace_ptr f) {
-    /* What info should be created for virtual entities */
+    MFace_DownAdj_RN *downadj;
+
+    downadj = f->downadj = (MFace_DownAdj_RN *) MSTK_malloc(sizeof(MFace_DownAdj_RN));
+    downadj->fvertices = NULL;
   }
 
 
   void MF_Delete_R1(MFace_ptr f, int keep) {
-    MSTK_Report("MF_Delete_R1","Not Implemented",ERROR);
+    MFace_DownAdj_RN *downadj;
+
+    downadj = (MFace_DownAdj_RN *) f->downadj;
+
+    if (downadj) {
+      if (downadj->fvertices) List_Delete(downadj->fvertices);
+      MSTK_free(downadj);
+    }
+
+    if (keep) 
+      MSTK_Report("MF_Delete_R1","Deletion of faces is permanent in this representation",ERROR);
   }
 
-  void MF_Destroy_For_MESH_Delete_R1(MFace_ptr f, int keep) {
-    MSTK_Report("MF_Destroy_For_MESH_Delete_R1","Not Implemented",ERROR);
+  void MF_Restore_R1(MFace_ptr f) {
+#ifdef DEBUG
+    MSTK_Report("MF_Restore_R1",
+		"Function call not suitable for this representation",WARN);
+#endif
+  }
+
+  void MF_Destroy_For_MESH_Delete_R1(MFace_ptr f) {
+#ifdef DEBUG
+    MSTK_Report("MF_Destroy_For_MESH_Delete_R1",
+		"Function call not suitable for this representation",ERROR);
+#endif
+  }
+
+  int MF_Set_GInfo_Auto_R1(MFace_ptr f) {
+    return MF_Set_GInfo_Auto_RN(f);
   }
 
   void MF_Set_Edges_R1(MFace_ptr f, int n, MEdge_ptr *e, int *dir) {
 #ifdef DEBUG
-    MSTK_Report("MF_Set_Edges","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Set_Edges_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void MF_Replace_Edges_R1(MFace_ptr f, int nold, MEdge_ptr *oldedges, int nnu, MEdge_ptr *nuedges) {
 #ifdef DEBUG
-    MSTK_Report("MF_Replace_Edges","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Replace_Edges_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void MF_Replace_Edges_i_R1(MFace_ptr f, int nold, int i, int nnu, MEdge_ptr *nuedges) {
 #ifdef DEBUG
-    MSTK_Report("MF_Replace_Edges_i","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Replace_Edges_i_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void MF_Set_Vertices_R1(MFace_ptr f, int n, MVertex_ptr *v) {
-    MSTK_Report("MF_Set_Vertices","Not implemented for this representation",WARN);
+    MF_Set_Vertices_RN(f,n,v);
   }
 
   void MF_Replace_Vertex_R1(MFace_ptr f, MVertex_ptr v, MVertex_ptr nuv) {
-    MSTK_Report("MF_Replace_Vertex","Not implemented for this representation",WARN);
+#ifdef DEBUG
+    MSTK_Report("MF_Replace_Vertex_R1","Modifying a temporary entity",WARN);
+#endif
+    MF_Replace_Vertex_RN(f,v,nuv);
   }
 
   void MF_Replace_Vertex_i_R1(MFace_ptr f, int i, MVertex_ptr nuv) {
-    MSTK_Report("MF_Replace_Vertex","Not implemented for this representation",WARN);
+#ifdef DEBUG
+    MSTK_Report("MF_Replace_Vertex_i_R1","Modifying a temporary entity",WARN);
+#endif
+    MF_Replace_Vertex_i_RN(f,i,nuv);
   }
 
   void MF_Insert_Vertex_R1(MFace_ptr f, MVertex_ptr nuv, MVertex_ptr b4v) {
-    MSTK_Report("MF_Insert_Vertex_R1","Not implemented for this representation",WARN);
+#ifdef DEBUG
+    MSTK_Report("MF_Insert_Vertex_R1","Modifying a temporary entity",WARN);
+#endif
+    MF_Insert_Vertex_RN(f,nuv,b4v);
   }
 
   void MF_Insert_Vertex_i_R1(MFace_ptr f, MVertex_ptr nuv, int i) {
-    MSTK_Report("MF_Insert_Vertex_i_R1","Not implemented for this representation",WARN);
+#ifdef DEBUG
+    MSTK_Report("MF_Insert_Vertex_i_R1","Modifying a temporary entity",WARN);
+#endif
+    MF_Insert_Vertex_i_RN(f,nuv,i);
   }
 
   void MF_Add_Region_R1(MFace_ptr f, MRegion_ptr r, int side) {
 #ifdef DEBUG
-    MSTK_Report("MF_Add_Region_R1","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Add_Region_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void MF_Rem_Region_R1(MFace_ptr f, MRegion_ptr r) {
 #ifdef DEBUG
-    MSTK_Report("MF_Rem_Region_R1","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Rem_Region_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
+  int MFs_AreSame_R1(MFace_ptr f1, MFace_ptr f2) {
+    if (f1 == f2)
+      return 1;
+    else
+      return MFs_AreSame_R1R2(f1,f2);
+  }
+
   int MF_Num_Vertices_R1(MFace_ptr f) {
-    return MF_Num_Vertices_R1R2(f);
+    return MF_Num_Vertices_RN(f);
   }
 
   int MF_Num_Edges_R1(MFace_ptr f) {
-    return MF_Num_Edges_R1R2(f);
+    return MF_Num_Edges_RN(f);
   }
 
   List_ptr MF_Vertices_R1(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Vertices_R1R2(f,dir,v0);
+    return MF_Vertices_RN(f,dir,v0);
   }
 	
 
   List_ptr MF_Edges_R1(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Edges_R1R2(f,dir,v0); 
+    return MF_Edges_RN(f,dir,v0); 
   }
 
   int MF_EdgeDir_R1(MFace_ptr f, MEdge_ptr e) {
-    return MF_EdgeDir_R1R2(f,e);
+    return MF_EdgeDir_RN(f,e);
   }
 
   int MF_EdgeDir_i_R1(MFace_ptr f, int i) {
-    return MF_EdgeDir_i_R1R2(f,i);
+    return MF_EdgeDir_i_RN(f,i);
   }
 
   int MF_UsesEdge_R1(MFace_ptr f, MEdge_ptr e) {
-    return MF_UsesEdge_R1R2(f,e);
+    return MF_UsesEdge_RN(f,e);
   }
 
   int MF_UsesVertex_R1(MFace_ptr f, MVertex_ptr v) {
-    return MF_UsesVertex_R1R2(f,v);
+    return MF_UsesVertex_RN(f,v);
   }
 
   List_ptr MF_Regions_R1(MFace_ptr f) {
@@ -114,24 +165,28 @@ extern "C" {
   }
 
   int MF_Num_AdjFaces_R1(MFace_ptr f) {
-    MSTK_Report("MF_Num_AdjFaces_R1","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_Num_AdjFaces_R1",
+		"Not yet implemented for this representation",WARN);
     return 0;
   }
 
   List_ptr MF_AdjFaces_R1(MFace_ptr f) {
-    MSTK_Report("MF_AdjFaces_R1","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_AdjFaces_R1",
+		"Not yet implemented for this representation",WARN);
     return 0;
   }
 
   void MF_Add_AdjFace_R1(MFace_ptr f, int side, MFace_ptr af) {
 #ifdef DEBUG
-    MSTK_Report("MF_Add_AdjFace_R1","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Add_AdjFace_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void MF_Rem_AdjFace_R1(MFace_ptr f, int side, MFace_ptr af) {
 #ifdef DEBUG
-    MSTK_Report("MF_Rem_AdjFace_R1","Function call not suitable for this representation",WARN);
+    MSTK_Report("MF_Rem_AdjFace_R1",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
--- src/base/MFace_R4.c
+++ src/base/MFace_R4.c
@@ -12,43 +12,32 @@ extern "C" {
 
   void MF_Set_RepType_R4(MFace_ptr f) {
     MFace_UpAdj_R3R4 *upadj;
-    MFace_DownAdj_R3R4 *downadj;
+    MFace_DownAdj_RN *downadj;
 
     upadj = f->upadj =(MFace_UpAdj_R3R4 *) MSTK_malloc(sizeof(MFace_UpAdj_R3R4));
     upadj->fregions[0] = (MRegion_ptr) NULL;
     upadj->fregions[1] = (MRegion_ptr) NULL;
-    downadj = (MFace_DownAdj_R3R4 *) MSTK_malloc(sizeof(MFace_DownAdj_R3R4));
-    downadj->nv = 0;
+    downadj = (MFace_DownAdj_RN *) MSTK_malloc(sizeof(MFace_DownAdj_RN));
     downadj->fvertices = NULL;
   }
 
   void MF_Delete_R4(MFace_ptr f, int keep) {
     MFace_UpAdj_R3R4 *upadj;
-    MFace_DownAdj_R3R4 *downadj;
-    int i, nv;
+    MFace_DownAdj_RN *downadj;
+    int idx;
     MVertex_ptr v;
 
-    downadj = (MFace_DownAdj_R3R4 *) f->downadj;
+    downadj = (MFace_DownAdj_RN *) f->downadj;
 
-    if (f->dim != MDELFACE) { /* if face has not been temporarily deleted */
+    if (MEnt_Dim(f) != MDELETED) { /* if face hasnt been temporarily deleted */
       if (downadj) {
-	nv = downadj->nv;
-	for (i = 0; i < nv; i++) {
-	  v = List_Entry(downadj->fvertices,i);
+	idx = 0;
+	while ((v = List_Next_Entry(downadj->fvertices,&idx))) 
 	  MV_Rem_Face(v,f);
-	}
       }
     }
 
-    if (keep) {
-      MSTK_KEEP_DELETED = 1;
-      f->dim = MDELFACE;
-    }
-    else {
-#ifdef DEBUG
-      f->dim = MDELFACE;
-#endif
-
+    if (!keep) {
       upadj  = (MFace_UpAdj_R3R4 *) f->upadj;
       if (upadj)
 	MSTK_free(upadj);
@@ -58,45 +47,40 @@ extern "C" {
 	  List_Delete(downadj->fvertices);
 	MSTK_free(downadj);
       }
-
-      MSTK_free(f);
     }
   }
 
   void MF_Restore_R4(MFace_ptr f) {
-    MFace_DownAdj_R3R4 *downadj;
-    int i, nv;
+    MFace_DownAdj_RN *downadj;
+    int idx;
     MVertex_ptr v;
 
-    if (f->dim != MDELFACE)
-      return;
+    MEnt_Set_Dim(f,MFACE);
 
-    f->dim = MFACE;
-
-    downadj = (MFace_DownAdj_R3R4 *) f->downadj;
-    nv = downadj->nv;
-    for (i = 0; i < nv; i++) {
-      v = List_Entry(downadj->fvertices,i);
+    downadj = (MFace_DownAdj_RN *) f->downadj;
+    idx = 0;
+    while ((v = List_Next_Entry(downadj->fvertices,&idx))) 
       MV_Add_Face(v,f);
-    }
   }
   
   void MF_Destroy_For_MESH_Delete_R4(MFace_ptr f) {
     MFace_UpAdj_R3R4 *upadj;
-    MFace_DownAdj_R3R4 *downadj;
+    MFace_DownAdj_RN *downadj;
 
     upadj  = (MFace_UpAdj_R3R4 *) f->upadj;
     if (upadj)
       MSTK_free(upadj);
 
-    downadj = (MFace_DownAdj_R3R4 *) f->downadj;
+    downadj = (MFace_DownAdj_RN *) f->downadj;
     if (downadj) {
       if (downadj->fvertices)
 	List_Delete(downadj->fvertices);
       MSTK_free(downadj);
     }
+  }
 
-    MSTK_free(f);
+  int MF_Set_GInfo_Auto_R4(MFace_ptr f) {
+    return MF_Set_GInfo_Auto_RN(f);
   }
 
   void MF_Set_Edges_R4(MFace_ptr f, int n, MEdge_ptr *e, int *dir) {
@@ -121,64 +105,70 @@ extern "C" {
   }
 
   void MF_Set_Vertices_R4(MFace_ptr f, int n, MVertex_ptr *v) {
-    MF_Set_Vertices_R3R4(f,n,v);
+    MF_Set_Vertices_RN(f,n,v);
   }
 
   void MF_Replace_Vertex_R4(MFace_ptr f, MVertex_ptr v, MVertex_ptr nuv) {
-    MF_Replace_Vertex_R3R4(f,v,nuv);
+    MF_Replace_Vertex_RN(f,v,nuv);
   }
 
   void MF_Replace_Vertex_i_R4(MFace_ptr f, int i, MVertex_ptr nuv) {
-    MF_Replace_Vertex_i_R3R4(f,i,nuv);
+    MF_Replace_Vertex_i_RN(f,i,nuv);
   }
 
   void MF_Insert_Vertex_R4(MFace_ptr f, MVertex_ptr nuv, MVertex_ptr b4v) {
-    MF_Insert_Vertex_R3R4(f,nuv,b4v);
+    MF_Insert_Vertex_RN(f,nuv,b4v);
   }
 
   void MF_Insert_Vertex_i_R4(MFace_ptr f, MVertex_ptr nuv, int i) {
-    MF_Insert_Vertex_i_R3R4(f,nuv,i);
+    MF_Insert_Vertex_i_RN(f,nuv,i);
   }
 
   void MF_Add_Region_R4(MFace_ptr f, MRegion_ptr r, int side) {
-    MSTK_Report("MF_Add_Region_R2", "Not Implemented for this representation",WARN);
+    MSTK_Report("MF_Add_Region_R2", 
+		"Not yet implemented for this representation",WARN);
   }
 
   void MF_Rem_Region_R4(MFace_ptr f, MRegion_ptr r) {
-    MSTK_Report("MF_Rem_Region_R2","Not Implemented for this representation",WARN);
+    MSTK_Report("MF_Rem_Region_R2",
+		"Not yet implemented for this representation",WARN);
+  }
+
+  int MFs_AreSame_R4(MFace_ptr f1, MFace_ptr f2) {
+    return (f1 == f2);
   }
 
   int MF_Num_Vertices_R4(MFace_ptr f) {
-    return MF_Num_Vertices_R3R4(f);
+    return MF_Num_Vertices_RN(f);
   }
 
   int MF_Num_Edges_R4(MFace_ptr f) {
-    return MF_Num_Edges_R3R4(f);
+    return MF_Num_Edges_RN(f);
   }
 
   List_ptr MF_Vertices_R4(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Vertices_R3R4(f,dir,v0);
+    return MF_Vertices_RN(f,dir,v0);
   }
 	
 
   List_ptr MF_Edges_R4(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Edges_R3R4(f,dir,v0); 
+    return MF_Edges_RN(f,dir,v0); 
   }
 
   int MF_EdgeDir_R4(MFace_ptr f, MEdge_ptr e) {
-    return MF_EdgeDir_R3R4(f,e);
+    return MF_EdgeDir_RN(f,e);
   }
 
   int MF_EdgeDir_i_R4(MFace_ptr f, int i) {
-    return MF_EdgeDir_i_R3R4(f,i);
+    return MF_EdgeDir_i_RN(f,i);
   }
 
   int MF_UsesEdge_R4(MFace_ptr f, MEdge_ptr e) {
-    return MF_UsesEdge_R3R4(f,e);
+    return MF_UsesEdge_RN(f,e);
   }
 
   int MF_UsesVertex_R4(MFace_ptr f, MVertex_ptr v) {
-    return MF_UsesVertex_R3R4(f,v);
+    return MF_UsesVertex_RN(f,v);
   }
 
   List_ptr MF_Regions_R4(MFace_ptr f) {
@@ -191,24 +181,28 @@ extern "C" {
 
 
   int MF_Num_AdjFaces_R4(MFace_ptr f) {
-    MSTK_Report("MF_Num_AdjFaces_R1","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_Num_AdjFaces_R1",
+		"Not yet implemented for this representation",WARN);
     return 0;
   }
 
   List_ptr MF_AdjFaces_R4(MFace_ptr f) {
-    MSTK_Report("MF_AdjFaces_R1","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_AdjFaces_R1",
+		"Not yet implemented for this representation",WARN);
     return 0;
   }
 
   void MF_Add_AdjFace_R4(MFace_ptr f, int side, MFace_ptr af) {
 #ifdef DEBUG
-    MSTK_Report("MF_Add_AdjFace_R2","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_Add_AdjFace_R2",
+		"Not yet implemented for this representation",WARN);
 #endif
   }
 
   void MF_Rem_AdjFace_R4(MFace_ptr f, int side, MFace_ptr af) {
 #ifdef DEBUG
-    MSTK_Report("MF_Rem_AdjFace_R2","Not yet implemented for this representation",WARN);
+    MSTK_Report("MF_Rem_AdjFace_R2",
+		"Not yet implemented for this representation",WARN);
 #endif
   }
 
