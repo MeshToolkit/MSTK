662d4538b7cf799e79c77194f27d1165884a1c69
rao <devnull@localhost>
2004-11-05 16:02:14 -0700

    Replaced calls to functions called ***_R1R2 with **_RN

--- src/base/MEdge_R1.c
+++ src/base/MEdge_R1.c
@@ -10,15 +10,15 @@ extern "C" {
 #endif
 
   void ME_Set_RepType_R1(MEdge_ptr e) {
-    MSTK_Report("ME_Set_RepType_R1","Not implemented",ERROR);
+    return;
   }
 
   void ME_Delete_R1(MEdge_ptr e, int keep) {
-    MSTK_Report("ME_Delete_R1","Not implemented",ERROR);
+    return;
   }
 
   void ME_Destroy_For_MESH_Delete_R1(MEdge_ptr e) {
-    MSTK_Report("ME_Delete_R1","Not implemented",ERROR);
+    return;
   }
 
   int ME_Num_Faces_R1(MEdge_ptr e) {
@@ -30,34 +30,38 @@ extern "C" {
   }
 
   int ME_Num_Regions_R1(MEdge_ptr e) {
-    return ME_Num_Regions_R1R2(e);
+    return ME_Num_Regions_RN(e);
   }
 
   List_ptr ME_Regions_R1(MEdge_ptr e) {
-    return ME_Regions_R1R2(e);
+    return ME_Regions_RN(e);
   }
 
   void ME_Add_Face_R1(MEdge_ptr e, MFace_ptr f) {
 #ifdef DEBUG
-    MSTK_Report("ME_Add_Face","Function call not suitable for this representation",WARN);
+    MSTK_Report("ME_Add_Face",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
   
   void ME_Rem_Face_R1(MEdge_ptr e, MFace_ptr f) {
 #ifdef DEBUG
-    MSTK_Report("ME_Rem_Face","Function call not suitable for this representation",WARN);
+    MSTK_Report("ME_Rem_Face",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void ME_Add_Region_R1(MEdge_ptr e, MRegion_ptr r) {
 #ifdef DEBUG
-    MSTK_Report("ME_Add_Region","Function call not suitable for this representation",WARN);
+    MSTK_Report("ME_Add_Region",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
   void ME_Rem_Region_R1(MEdge_ptr e, MRegion_ptr r) {
 #ifdef DEBUG
-    MSTK_Report("ME_Rem_Region","Function call not suitable for this representation",WARN);
+    MSTK_Report("ME_Rem_Region",
+		"Function call not suitable for this representation",WARN);
 #endif
   }
 
