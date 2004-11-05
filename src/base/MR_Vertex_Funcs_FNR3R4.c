f41eb1db6d7c4859bbe364a4f2c3a6a3b03df456
rao <devnull@localhost>
2004-11-05 16:37:24 -0700

    Common region-vertex functionality

---
+++ src/base/MR_Vertex_Funcs_FNR3R4.c
@@ -0,0 +1,209 @@
+#define _H_MRegion_Private
+
+#include "MRegion.h"
+#include "MRegion_jmp.h"
+#include "MSTK_private.h"
+#include "MSTK_malloc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+  List_ptr MR_Vertices_FNR3R4(MRegion_ptr r) {
+    int i, j, n, nf, mkr, found, diradj0=0, diropp=0, edir, fdir, fdir0, fecheck;
+    MFace_ptr face=NULL, face0=NULL, fadj0=NULL, fopp=NULL;
+    MEdge_ptr edge, fedge00, upedge;
+    MVertex_ptr vert, rv0, rvopp0=NULL;
+    List_ptr rvertices, fverts, fedges0, adjfedges;
+    MRegion_DownAdj_FN *downadj;
+
+    downadj = (MRegion_DownAdj_FN *) r->downadj;
+
+    nf = List_Num_Entries(downadj->rfaces);
+    switch (nf) {
+    case 4: /* Tet! */
+      /* Add vertices of first face to list of region vertices */
+      face0 = List_Entry(downadj->rfaces,0); /* first face */
+      fdir0 = downadj->fdirs[0] & 1;    /* Sense in which face is used in region */
+      rvertices = MF_Vertices(face0,!fdir0,0);
+      n = 3;
+
+      face = List_Entry(downadj->rfaces,1);
+      fverts = MF_Vertices(face,1,0);
+      for (i = 0; i < 3 && n < 4; i++) { 
+	vert = List_Entry(fverts,i);
+	if (!List_Contains(rvertices,vert)) {
+	  List_Add(rvertices,vert);
+	  n++;
+	}
+      }
+      List_Delete(fverts);
+
+      return rvertices;
+      break;
+    case 6: /* Hex ? */
+
+      /* All faces must have 4 edges each */
+      fecheck = 1;
+      for (i = 0; i < nf; i++) {
+	face = List_Entry(downadj->rfaces,i);
+	if (MF_Num_Edges(face) != 4)
+	  fecheck = 0;
+      }
+
+      if (!fecheck)
+	break;
+
+      face0 = List_Entry(downadj->rfaces,0); /* face 0 */
+      fdir0 = downadj->fdirs[0] & 1; /* dir of face w.r.t. region */
+
+      
+      /* Add vertices of first face */
+      rvertices = MF_Vertices(face0,!fdir0,0);
+
+      /* vertex 0 of region and of face 0 */
+      rv0 = List_Entry(rvertices,0);      
+      
+      /* Edges of face 0 */
+      fedges0 = MF_Edges(face0,!fdir0,rv0);
+
+      /* edge 0 of face 0 (wrt face dir pointing into the region) */
+      fedge00 = List_Entry(fedges0,0); 
+
+      /* Get the face adjacent to edge 0 of face 0 and also the
+	 opposite face, a face that has no edge common face 0 */
+
+      fopp = NULL; fadj0 = NULL;
+      for (i = 1; i < nf; i++) {
+	face = List_Entry(downadj->rfaces,i);
+
+	/* Check if face uses any edge of face 0 */
+
+	for (j = 0, found = 0; j < 4; j++) {
+	  edge = List_Entry(fedges0,j);
+
+	  if (MF_UsesEntity(face,edge,1)) {
+	    if (edge == fedge00) {
+	      /* face uses edge 0 of face 0 (w.r.t. face dir pointing
+		 into region) */
+	      fadj0 = face;
+	      diradj0 = (downadj->fdirs[0])>>i & 1;  
+	    }
+	    found = 1;
+	    break;
+	  }
+	}
+	if (!found) {
+	  fopp = face;
+	  diropp = (downadj->fdirs[0])>>i & 1;
+	}
+	if (fopp && fadj0)
+	  break;
+      }
+
+      if (!fopp) {
+	MSTK_Report("MR_Vertices_FNR3R4","Could not find opposite face",ERROR);
+	List_Delete(fedges0);
+	List_Delete(rvertices);
+	return (void *) NULL;
+      }
+
+      /* edges of face adjacent to edge 0 of face 0 */
+      adjfedges = MF_Edges(fadj0,1,rv0);
+
+      /* edge connecting rv0 and vertex on opposite face */
+      upedge = diradj0 ? List_Entry(adjfedges,3): List_Entry(adjfedges,0);
+
+#ifdef DEBUG
+      if (!ME_UsesEntity(upedge,rv0,0))
+	MSTK_Report("MR_Vertices_FNR3R4","Cannot find correct vertical edge",
+		    ERROR);
+#endif
+	
+      if (ME_Vertex(upedge,0) == rv0)
+	rvopp0 = ME_Vertex(upedge,1);
+      else {
+	rvopp0 = ME_Vertex(upedge,0);
+      }
+
+      List_Delete(fedges0);
+      List_Delete(adjfedges);
+
+      fverts = MF_Vertices(fopp,diropp,rvopp0);
+
+      for (i = 0; i < 4; i++)
+	List_Add(rvertices,List_Entry(fverts,i));
+      List_Delete(fverts);
+
+      return rvertices;
+      break;
+    default: 
+      break;
+    }
+
+
+    /* Pyramids, Prisms, General Polyhedra */
+    /* We should do separate procedures for pyramids and prisms */
+    mkr = MSTK_GetMarker();
+    
+    /* Add vertices of first face */
+    face = List_Entry(downadj->rfaces,0); /* first face */
+    fdir = downadj->fdirs[0] & 1;    /* Sense in which face is used in region */
+    
+    rvertices = MF_Vertices(face,!fdir,0); 
+    List_Mark(rvertices,mkr);
+    
+    for (i = 1; i < nf-1; i++) {
+      face = List_Entry(downadj->rfaces,i);
+      fverts = MF_Vertices(face,1,0);
+      n = List_Num_Entries(fverts);
+      for (j = 0; j < n; j++) {
+	vert = List_Entry(fverts,j);
+	if (!MEnt_IsMarked(vert,mkr)) {
+	  List_Add(rvertices,vert);
+	  MEnt_Mark(vert,mkr);
+	}
+      }
+      List_Delete(fverts);
+    }
+    List_Unmark(rvertices,mkr);
+    MSTK_FreeMarker(mkr);
+    
+    return rvertices;
+  }
+
+  void MR_Replace_Vertex_FNR3R4(MRegion_ptr r, MVertex_ptr v, MVertex_ptr nuv){
+#ifdef DEBUG
+    MSTK_Report("MR_Replace_Vertex",
+		"Function call not suitable for this representation",WARN);
+#endif
+  }
+
+  void MR_Replace_Vertex_i_FNR3R4(MRegion_ptr r, int i, MVertex_ptr nuv) {
+#ifdef DEBUG
+    MSTK_Report("MF_Replace_Vertex_i",
+		"Function call not suitable for this representation",WARN);
+#endif
+  }
+
+
+  int MR_UsesVertex_FNR3R4(MRegion_ptr r, MVertex_ptr v) {
+    int i, nf;
+    MFace_ptr face;
+    MRegion_DownAdj_FN *downadj;
+
+    downadj = (MRegion_DownAdj_FN *) r->downadj;
+    nf = List_Num_Entries(downadj->rfaces);
+
+    for (i = 0; i < nf; i++) {
+      face = List_Entry(downadj->rfaces,i);
+      if (MF_UsesEntity(face,v,MVERTEX))
+	return 1;
+    }
+    return 0;
+  }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/base/MR_Vertex_Funcs_R1R2.c
@@ -0,0 +1,121 @@
+#define _H_MRegion_Private
+
+#include "MRegion.h"
+#include "MRegion_jmp.h"
+#include "MSTK_private.h"
+#include "MSTK_malloc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int MR_Set_GInfo_Auto_R1R2(MRegion_ptr r) {
+    int i, same, nv, rgdim, rgid, vgdim, vgid, vgdim0, vgid0;
+    MVertex_ptr v;
+    MRegion_DownAdj_R1R2 *downadj;
+
+    downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    nv = List_Num_Entries(downadj->rvertices);
+
+    same = 1;
+    rgdim = -1;
+    rgid = -1;
+
+    v = List_Entry(downadj->rvertices,0);    
+    vgid0 = MV_GEntID(v);
+    vgdim0 = MV_GEntDim(v);
+
+    for (i = 1; i < nv; i++) {
+      v = List_Entry(downadj->rvertices,i);
+      vgid = MV_GEntID(v);
+      vgdim = MV_GEntDim(v);
+      if (vgdim == vgdim0 && vgid == vgid0)
+	continue; /* all vertices have same classification so far */
+      else {
+	same = 0;
+	if (vgdim > rgdim) {
+	  rgdim = vgdim;
+	  rgid = vgid;
+	}
+      }
+    }
+    if (same) {
+      rgdim = vgdim0;
+      rgid = vgid;
+    }
+     
+    if (rgdim == -1)
+      rgdim = 4;
+    MEnt_Set_GEntDim(r,rgdim);
+    MEnt_Set_GEntID(r,rgid);
+
+    if (rgdim == 4)
+      return 0;
+    else
+      return 1;
+  }
+
+  void MR_Set_Vertices_R1R2(MRegion_ptr r, int nv, MVertex_ptr *rvertices, 
+			    int nf, int **rfvtemplate) {
+    int i;
+    MRegion_DownAdj_R1R2 *downadj;
+
+    downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    downadj->rvertices = List_New(nv);
+    for (i = 0; i < nv; i++)
+      List_Add(downadj->rvertices,rvertices[i]);
+
+    /* If this is a non-standard element and the face vertex template
+       has been specified store this information for later
+       retrieval */
+    if (nf && rfvtemplate) {
+      downadj->fvtemplate = (int **) MSTK_malloc((nf+1)*sizeof(int *));
+      downadj->fvtemplate[0] = (int *) MSTK_malloc(1*sizeof(int));
+      for (i = 0; i < nf; i++) {
+	int j, nfv;
+	nfv = rfvtemplate[i][0];
+	downadj->fvtemplate[i] = (int *) MSTK_malloc((nfv+1)*sizeof(int));
+	downadj->fvtemplate[i][0] = nfv;
+	for (j = 0; j < nfv; j++)
+	  downadj->fvtemplate[i][j+1] = rfvtemplate[i][j+1];
+      }
+    }
+  }
+
+
+  List_ptr MR_Vertices_R1R2(MRegion_ptr r) {
+    MRegion_DownAdj_R1R2 *downadj;
+    downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    return List_Copy(downadj->rvertices);
+  }
+
+
+  int MR_UsesVertex_R1R2(MRegion_ptr r, MVertex_ptr v) {
+    MRegion_DownAdj_R1R2 *downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    return List_Contains(downadj->rvertices,v);
+  }
+
+  void MR_Replace_Vertex_R1R2(MRegion_ptr r, MVertex_ptr v, MVertex_ptr nuv) {
+    int i, nv;
+    MRegion_DownAdj_R1R2 *downadj;
+
+    downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    nv = List_Num_Entries(downadj->rvertices);
+    for (i = 0; i < nv; i++)
+      if (v == (MVertex_ptr) List_Entry(downadj->rvertices,i)) {
+	List_Replacei(downadj->rvertices,i,nuv);
+	return;
+      }
+  }
+
+  void MR_Replace_Vertex_i_R1R2(MRegion_ptr r, int i, MVertex_ptr nuv) {
+    MRegion_DownAdj_R1R2 *downadj;
+
+    downadj = (MRegion_DownAdj_R1R2 *) r->downadj;
+    List_Replacei(downadj->rvertices,i,nuv);
+  }
+
+
+#ifdef __cplusplus
+}
+#endif
