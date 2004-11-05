f44af143153c5b9610841987b848e80a1d93773a
rao <devnull@localhost>
2004-11-05 16:07:01 -0700

    Lumped all ME_Region functions for reduced representations together

---
+++ src/base/ME_Region_Funcs_RN.c
@@ -0,0 +1,54 @@
+#define _H_MEdge_Private
+
+#include "MSTK_malloc.h"
+#include "MEdge.h"
+#include "MEdge_jmp.h"
+#include "MSTK_private.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int ME_Num_Regions_RN(MEdge_ptr e) {
+    int ne, idx;
+    MRegion_ptr reg;
+    List_ptr vregs;
+
+    ne = 0;
+
+    vregs = MV_Regions(e->vertex[0]);
+    idx = 0;
+    while ((reg = List_Next_Entry(vregs,&idx))) {
+      if (MR_UsesEntity(reg,e->vertex[1],MVERTEX))
+	ne++;
+    }
+
+    return ne;
+  }
+
+  List_ptr ME_Regions_RN(MEdge_ptr e) {
+    int ne, idx;
+    MRegion_ptr reg;
+    List_ptr eregs, vregs;
+
+    eregs = List_New(0);
+
+    vregs = MV_Regions(e->vertex[0]);
+    idx = 0;
+    while ((reg = List_Next_Entry(vregs,&idx))) {
+      if (MR_UsesEntity(reg,e->vertex[1],MVERTEX))
+	List_Add(eregs,reg);
+    }
+
+    if (List_Num_Entries(eregs))
+      return eregs;    
+    else {
+      List_Delete(eregs);
+      return NULL;
+    }
+  }
+
+#ifdef __cplusplus
+}
+#endif
+
