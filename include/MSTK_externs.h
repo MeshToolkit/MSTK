c5ff317d341df3b1b56239b07b526ecbfa871d82
rao <devnull@localhost>
2004-11-08 11:17:51 -0700

    Contains region-vertex, region-edge and region-face templates for common element types. These templates should be available by including MSTK.h

---
+++ include/MSTK_externs.h
@@ -0,0 +1,33 @@
+#ifndef _H_MSTK_EXTERN
+#define _H_MSTK_EXTERN
+
+/* If the compiler complains, change first line to 
+   #ifndef _H_MSTK_EXTERN && _H_MSTK_GLOBALVARS
+*/
+
+#include "MSTK_types.h"
+
+/* Templates for TETS, PYRAMIDS, PRISMS and HEXS (move to global file?) */
+
+/* Number of faces for standard regions */
+extern int const MSTK_nrf_template[5];
+
+/* Number of edges for standard regions */
+extern int const MSTK_nre_template[5];
+
+/* Translate number of vertices to standard region types */
+extern const MRType MSTK_nv2rtype[9];
+
+/* Face-vertex templates for standard regions */
+extern const int MSTK_rfv_template[5][6][5];
+
+/* Face direction templates for standard regions */
+extern const int MSTK_rfdir_template[5][6];
+
+/* Face-edge templates for standard regions */
+extern const int MSTK_rfe_template[5][6][5]; 
+
+/* Edge-vertex templates for standard regions */
+extern const int MSTK_rev_template[5][12][2];
+
+#endif
