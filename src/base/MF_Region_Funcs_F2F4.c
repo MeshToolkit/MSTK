4c77893e8eb624d431af36ab09a2894371439b00
rao <devnull@localhost>
2010-02-25 13:24:21 -0700

    Face-->region routines common to F2 and F4 representations. Had to pull them out because of data structure changes

---
+++ src/base/MF_Region_Funcs_F2F4.c
@@ -0,0 +1,106 @@
+#define _H_MFace_Private
+
+#include "MFace.h"
+#include "MSTK_private.h"
+#include "MSTK_malloc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+  List_ptr MF_Regions_F2F4(MFace_ptr f) {
+    List_ptr fregs, eregs;
+    MRegion_ptr r;
+    MEdge_ptr e;
+    MFace_Adj_F2F4 *adj;
+    int i, k=0, nr;
+    
+    adj = (MFace_Adj_F2F4 *) f->adj;
+    
+    fregs = List_New(2);
+    e = List_Entry(adj->fedges,0);
+    
+    eregs = ME_Regions(e);
+    if (eregs) {
+      nr = List_Num_Entries(eregs);
+      
+      for (i = 0; i < nr; i++) {
+	r = List_Entry(eregs,i);
+	if (MR_UsesEntity(r,(MEntity_ptr) f,2)) {
+	  List_Add(fregs,r);
+	  k++;
+	}
+	if (k == 2)
+	  break;
+      }
+      
+      List_Delete(eregs);
+    }
+			
+    
+    if (k) 
+      return fregs;
+    else {
+      List_Delete(fregs);
+      return 0;
+    }
+  }
+
+  MRegion_ptr MF_Region_F2F4(MFace_ptr f, int ir) {
+    List_ptr eregs;
+    MRegion_ptr r, r1;
+    MEdge_ptr e;
+    int nr, i, dir, fdir;
+    MFace_Adj_F2F4 *adj;
+    
+#ifdef DEBUG
+    MSTK_Report("MF_Region_F4","More efficient to use MF_Regions",MESG);
+#endif
+
+    dir = (ir) ? 0 : 1;
+    
+    adj = (MFace_Adj_F2F4 *) f->adj;
+    e = List_Entry(adj->fedges,0);
+    
+    eregs = ME_Regions(e);
+    if (eregs) {
+      nr = List_Num_Entries(eregs);
+      
+      r1 = 0;
+      for (i = 0; i < nr; i++) {
+	r = List_Entry(eregs,i);
+	fdir = MR_FaceDir(r,f);
+	if (fdir == !dir) {
+	  r1 = r;
+	  break;
+	}
+      }
+      List_Delete(eregs);
+	
+      return r1;
+    }
+    
+    return 0;
+  }
+
+  void MF_Add_Region_F2F4(MFace_ptr f, MRegion_ptr r, int side) {
+#ifdef DEBUG
+    MSTK_Report("MF_Add_Region",
+		"Function call unsuitable for this representation",
+		WARN);
+#endif
+  }
+
+  void MF_Rem_Region_F2F4(MFace_ptr f, MRegion_ptr r) {
+#ifdef DEBUG
+    MSTK_Report("MF_Rem_Region",
+		"Function call unsuitable for this representation",
+		WARN);
+#endif
+  }
+
+
+#ifdef __cplusplus
+}
+#endif
