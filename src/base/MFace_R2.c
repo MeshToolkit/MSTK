67f43c24551719304d5755e2a6bd305e0950feea
rao <devnull@localhost>
2010-02-25 13:19:05 -0700

    Changed to storing all adjacencies in single field instead of downward, upward and same-level adjacencies separately. Needed to move some routines out to other files

--- src/base/MFace_R2.c
+++ src/base/MFace_R2.c
@@ -38,18 +38,14 @@ extern "C" {
   }
 
   void MF_Destroy_For_MESH_Delete_R2(MFace_ptr f) {
-    MFace_DownAdj_RN *downadj;
+    MFace_Adj_R1 *adj;
 
-    downadj = (MFace_DownAdj_RN *) f->downadj;
+    adj = (MFace_Adj_R1 *) f->adj;
 
-    if (downadj) {
-      if (downadj->fvertices) List_Delete(downadj->fvertices);
-      MSTK_free(downadj);
+    if (adj) {
+      if (adj->fvertices) List_Delete(adj->fvertices);
+      MSTK_free(adj);
     }
-  }
-
-  int MF_Set_GInfo_Auto_R2(MFace_ptr f) {
-    return MF_Set_GInfo_Auto_RN(f);
   }
 
   void MF_Set_Edges_R2(MFace_ptr f, int n, MEdge_ptr *e, int *dir) {
@@ -73,45 +69,6 @@ extern "C" {
 #endif
   }
 
-  void MF_Set_Vertices_R2(MFace_ptr f, int n, MVertex_ptr *v) {
-    MF_Set_Vertices_RN(f,n,v);
-  }
-
-  void MF_Replace_Vertex_R2(MFace_ptr f, MVertex_ptr v, MVertex_ptr nuv) {
-#ifdef DEBUG
-    MSTK_Report("MF_Replace_Vertex_R2","Modifying a temporary entity",WARN);
-#endif
-    MF_Replace_Vertex_RN(f,v,nuv);
-  }
-
-  void MF_Replace_Vertex_i_R2(MFace_ptr f, int i, MVertex_ptr v) {
-#ifdef DEBUG
-    MSTK_Report("MF_Replace_Vertex_i_R2","Modifying a temporary entity",WARN);
-#endif
-    MF_Replace_Vertex_i_RN(f,i,v);
-  }
-
-  void MF_Insert_Vertex_R2(MFace_ptr f, MVertex_ptr nuv, MVertex_ptr b4v) {
-#ifdef DEBUG
-    MSTK_Report("MF_Insert_Vertex_R2","Modifying a temporary entity",WARN);
-#endif
-    MF_Insert_Vertex_RN(f,nuv,b4v);
-  }
-
-  void MF_Insert_Vertex_i_R2(MFace_ptr f, MVertex_ptr nuv, int i) {
-#ifdef DEBUG
-    MSTK_Report("MF_Insert_Vertex_i_R2","Modifying a temporary entity",WARN);
-#endif
-    MF_Insert_Vertex_i_RN(f,nuv,i);
-  }
-
-  int MF_Rev_EdgeDir_R2(MFace_ptr f, MEdge_ptr e) {
-    return MF_Rev_EdgeDir_RN(f,e);
-  }
-
-  int MF_Rev_EdgeDir_i_R2(MFace_ptr f, int i) {
-    return MF_Rev_EdgeDir_i_RN(f,i);
-  }
 
   void MF_Add_Region_R2(MFace_ptr f, MRegion_ptr r, int side) {
     MSTK_Report("MF_Add_Region_R2", 
@@ -122,55 +79,6 @@ extern "C" {
     MSTK_Report("MF_Rem_Region_R2",
 		"Function call not suitable for this representation",WARN);
   }
-
-  int MFs_AreSame_R2(MFace_ptr f1, MFace_ptr f2) {
-    if (f1 == f2)
-      return 1;
-    else
-      return MFs_AreSame_R1R2(f1,f2);
-  }
-
-  int MF_Num_Vertices_R2(MFace_ptr f) {
-    return MF_Num_Vertices_RN(f);
-  }
-
-  int MF_Num_Edges_R2(MFace_ptr f) {
-    return MF_Num_Edges_RN(f);
-  }
-
-  List_ptr MF_Vertices_R2(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Vertices_RN(f,dir,v0);
-  }
-	
-
-  List_ptr MF_Edges_R2(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Edges_RN(f,dir,v0); 
-  }
-
-  int MF_EdgeDir_R2(MFace_ptr f, MEdge_ptr e) {
-    return MF_EdgeDir_RN(f,e);
-  }
-
-  int MF_EdgeDir_i_R2(MFace_ptr f, int i) {
-    return MF_EdgeDir_i_RN(f,i);
-  }
-
-  int MF_UsesEdge_R2(MFace_ptr f, MEdge_ptr e) {
-    return MF_UsesEdge_RN(f,e);
-  }
-
-  int MF_UsesVertex_R2(MFace_ptr f, MVertex_ptr v) {
-    return MF_UsesVertex_RN(f,v);
-  }
-
-  List_ptr MF_Regions_R2(MFace_ptr f) {
-    return MF_Regions_R1R2(f);
-  }
-
-  MRegion_ptr MF_Region_R2(MFace_ptr f, int dir) {
-    return MF_Region_R1R2(f,dir);
-  }
-
 
   int MF_Num_AdjFaces_R2(MFace_ptr f) {
     MSTK_Report("MF_Num_AdjFaces_R2",
--- src/base/MFace_R4.c
+++ src/base/MFace_R4.c
@@ -26,7 +26,7 @@ extern "C" {
 
     adj = (MFace_Adj_R4 *) f->adj;
 
-    if (MEnt_Dim(f) != MDELETED) { /* if face hasnt been temporarily deleted */
+    if (MEnt_Dim((MEntity_ptr) f) != MDELETED) { /* if face hasnt been temporarily deleted */
       if (adj) {
 	idx = 0;
 	while ((v = List_Next_Entry(adj->fvertices,&idx))) 
@@ -48,7 +48,7 @@ extern "C" {
     int idx;
     MVertex_ptr v;
 
-    MEnt_Set_Dim(f,MFACE);
+    MEnt_Set_Dim((MEntity_ptr) f,MFACE);
 
     adj = (MFace_Adj_R4 *) f->adj;
     idx = 0;
@@ -65,10 +65,6 @@ extern "C" {
 	List_Delete(adj->fvertices);
       MSTK_free(adj);
     }
-  }
-
-  int MF_Set_GInfo_Auto_R4(MFace_ptr f) {
-    return MF_Set_GInfo_Auto_RN(f);
   }
 
   void MF_Set_Edges_R4(MFace_ptr f, int n, MEdge_ptr *e, int *dir) {
@@ -92,89 +88,9 @@ extern "C" {
 #endif
   }
 
-  void MF_Set_Vertices_R4(MFace_ptr f, int n, MVertex_ptr *v) {
-    MF_Set_Vertices_RN(f,n,v);
-  }
-
-  void MF_Replace_Vertex_R4(MFace_ptr f, MVertex_ptr v, MVertex_ptr nuv) {
-    MF_Replace_Vertex_RN(f,v,nuv);
-  }
-
-  void MF_Replace_Vertex_i_R4(MFace_ptr f, int i, MVertex_ptr nuv) {
-    MF_Replace_Vertex_i_RN(f,i,nuv);
-  }
-
-  void MF_Insert_Vertex_R4(MFace_ptr f, MVertex_ptr nuv, MVertex_ptr b4v) {
-    MF_Insert_Vertex_RN(f,nuv,b4v);
-  }
-
-  void MF_Insert_Vertex_i_R4(MFace_ptr f, MVertex_ptr nuv, int i) {
-    MF_Insert_Vertex_i_RN(f,nuv,i);
-  }
-
-  int MF_Rev_EdgeDir_R4(MFace_ptr f, MEdge_ptr e) {
-    return MF_Rev_EdgeDir_RN(f,e);
-  }
-
-  int MF_Rev_EdgeDir_i_R4(MFace_ptr f, int i) {
-    return MF_Rev_EdgeDir_i_RN(f,i);
-  }
-
-  void MF_Add_Region_R4(MFace_ptr f, MRegion_ptr r, int side) {
-    MSTK_Report("MF_Add_Region_R4", 
-		"Not yet implemented for this representation",WARN);
-  }
-
-  void MF_Rem_Region_R4(MFace_ptr f, MRegion_ptr r) {
-    MSTK_Report("MF_Rem_Region_R4",
-		"Not yet implemented for this representation",WARN);
-  }
-
   int MFs_AreSame_R4(MFace_ptr f1, MFace_ptr f2) {
     return (f1 == f2);
   }
-
-  int MF_Num_Vertices_R4(MFace_ptr f) {
-    return MF_Num_Vertices_RN(f);
-  }
-
-  int MF_Num_Edges_R4(MFace_ptr f) {
-    return MF_Num_Edges_RN(f);
-  }
-
-  List_ptr MF_Vertices_R4(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Vertices_RN(f,dir,v0);
-  }
-	
-
-  List_ptr MF_Edges_R4(MFace_ptr f, int dir, MVertex_ptr v0) {
-    return MF_Edges_RN(f,dir,v0); 
-  }
-
-  int MF_EdgeDir_R4(MFace_ptr f, MEdge_ptr e) {
-    return MF_EdgeDir_RN(f,e);
-  }
-
-  int MF_EdgeDir_i_R4(MFace_ptr f, int i) {
-    return MF_EdgeDir_i_RN(f,i);
-  }
-
-  int MF_UsesEdge_R4(MFace_ptr f, MEdge_ptr e) {
-    return MF_UsesEdge_RN(f,e);
-  }
-
-  int MF_UsesVertex_R4(MFace_ptr f, MVertex_ptr v) {
-    return MF_UsesVertex_RN(f,v);
-  }
-
-  List_ptr MF_Regions_R4(MFace_ptr f) {
-    return MF_Regions_R3R4(f);
-  }
-
-  MRegion_ptr MF_Region_R4(MFace_ptr f, int dir) {
-    return MF_Region_R3R4(f,dir);
-  }
-
 
   int MF_Num_AdjFaces_R4(MFace_ptr f) {
     MSTK_Report("MF_Num_AdjFaces_R4",
