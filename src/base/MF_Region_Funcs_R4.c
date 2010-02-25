bad583df86ec05de1e9b00f19e45af9a270486fa
rao <devnull@localhost>
2010-02-25 13:25:40 -0700

    Face--->Region routines for reduced representation. Had to pull out separately due to data structure changes

---
+++ src/base/MF_Region_Funcs_R4.c
@@ -0,0 +1,62 @@
+#define _H_MFace_Private
+
+#include "MFace.h"
+#include "MSTK_private.h"
+#include "MSTK_malloc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+  List_ptr MF_Regions_R4(MFace_ptr f) {
+    List_ptr fregs;
+    MFace_Adj_R3 *adj;
+
+    adj = (MFace_Adj_R3 *) f->adj;
+
+    fregs = List_New(2);
+    if (adj->fregions[0])
+      List_Add(fregs,adj->fregions[0]);
+    if (adj->fregions[1])
+      List_Add(fregs,adj->fregions[1]);
+
+    return fregs;
+  }
+
+  MRegion_ptr MF_Region_R4(MFace_ptr f, int i) {
+    return ((MFace_Adj_R3 *) f->adj)->fregions[i];
+  }
+
+  void MF_Add_Region_R4(MFace_ptr f, MRegion_ptr r, int side) {
+    MFace_Adj_R3 *adj;
+
+    adj = (MFace_Adj_R3 *) f->adj;
+
+    if (side == MSTK_UNKNOWN) {
+      if (MR_FaceDir(r,f))
+	adj->fregions[0] = r;
+      else
+	adj->fregions[1] = r;
+    }
+    else
+      adj->fregions[side] = r;
+  }
+
+  void MF_Rem_Region_R4(MFace_ptr f, MRegion_ptr r) {
+    MFace_Adj_R3 *adj;
+
+    adj = (MFace_Adj_R3 *) f->adj;
+
+    if (adj->fregions[0] == r)
+      adj->fregions[0] = NULL;
+    else if (adj->fregions[1] == r)
+      adj->fregions[1] = NULL;
+    else
+      MSTK_Report("MF_Rem_Region_R4","Region not connected to face",ERROR);
+  }
+
+
+#ifdef __cplusplus
+}
+#endif
