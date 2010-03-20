3ac274a87a66649bc48d6c0ba1a8453eebc25840
rao <devnull@localhost>
2010-03-20 16:03:31 -0600

    Added support for VECTOR and TENSOR attributes. Added optional argument to MAttrib_New so that we can specify number of VECTOR or TENSOR components.

--- src/base/MAttrib.c
+++ src/base/MAttrib.c
@@ -1,6 +1,7 @@
 #define _H_MAttrib_Private
 
 #include <string.h>
+#include <stdarg.h>
 #include "MAttrib.h"
 #include "Mesh.h"
 #include "MSTK_private.h"
@@ -10,9 +11,10 @@ extern "C" {
 #endif
 
 
-  MAttrib_ptr MAttrib_New(Mesh_ptr mesh, const char *att_name, MAttType att_type, MType entdim) {
+  MAttrib_ptr MAttrib_New(Mesh_ptr mesh, const char *att_name, MAttType att_type, MType entdim, ...) {
     MAttrib_ptr attrib;
-    int i, natt;
+    int i, natt, ncomponents;
+    va_list ap;
 
 #ifdef DEBUG
     natt = MESH_Num_Attribs(mesh);
@@ -32,19 +34,40 @@ extern "C" {
     attrib->type = att_type;
     
 #ifdef DEBUG
-    if (att_type != INT && att_type != DOUBLE && att_type != POINTER) {
+    if (att_type != INT && att_type != DOUBLE && 
+	att_type != VECTOR && att_type != TENSOR && att_type != POINTER) {
       MSTK_Report("MAttDef_New","Unsupported attribute type",ERROR);
       return NULL;
     }
 #endif
 
     attrib->entdim = entdim; /* what type of entity can this be attached to? */
+
+    if (att_type == VECTOR || att_type == TENSOR) {
+      va_start(ap, entdim);
+
+      ncomponents = va_arg(ap, int);
+
+#ifdef DEBUG
+      if (ncomponents == 0)      
+	MSTK_Report("MAttrib_New","Number of components for attribute type VECTOR or TENSOR should be non-zero",FATAL);
+#endif
+      va_end(ap);
+    }
+
+    if (att_type == INT || att_type == DOUBLE || att_type == POINTER)
+      attrib->ncomp = 1;
+    else
+      attrib->ncomp = ncomponents;
     
     attrib->mesh = mesh;
     MESH_Add_Attrib(mesh,attrib);
 
+    va_end(ap);
+
     return attrib;
   }
+
 
   char *MAttrib_Get_Name(MAttrib_ptr attrib, char *att_name) {
     strcpy(att_name,attrib->name);
@@ -57,6 +80,10 @@ extern "C" {
 
   MType MAttrib_Get_EntDim(MAttrib_ptr attrib) {
     return attrib->entdim;
+  }
+
+  int MAttrib_Get_NumComps(MAttrib_ptr attrib) {
+    return attrib->ncomp;
   }
 
   void MAttrib_Delete(MAttrib_ptr attrib) {
@@ -74,7 +101,7 @@ extern "C" {
     attins = (MAttIns_ptr) MSTK_malloc(sizeof(MAttIns));
     
     attins->attrib = attrib;
-    attins->att_val.ival = 0; 
+    attins->att_val.pval = NULL;
 
     return attins;
   }
@@ -93,7 +120,7 @@ extern "C" {
     case DOUBLE:
       attins->att_val.lval = lval;
       break;
-    case POINTER:
+    case VECTOR: case TENSOR: case POINTER:
       attins->att_val.pval = pval;
       break;
     default:
@@ -112,9 +139,8 @@ extern "C" {
     case DOUBLE:
       *lval = attins->att_val.lval;
       break;
-    case POINTER:
+    case VECTOR: case TENSOR: case POINTER:
       *pval = attins->att_val.pval;
-      break;
     default:
       status = 0;
       break;
@@ -126,7 +152,7 @@ extern "C" {
   
   void MAttIns_Delete(MAttIns_ptr attins) {
     attins->attrib = NULL;
-    attins->att_val.ival = 0;
+    attins->att_val.pval = NULL;
     
     MSTK_free(attins);
   }
