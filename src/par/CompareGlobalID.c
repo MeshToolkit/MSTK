e244a669857ee61ce3ce437126a97e5e4745751d
duowang <devnull@localhost>
2012-03-26 15:50:04 -0600

    add two compare functions

--- src/par/CompareGlobalID.c
+++ src/par/CompareGlobalID.c
@@ -5,22 +5,41 @@
 extern "C" {
 #endif
 
-  int compareGlobalID(MEntity_ptr *a, MEntity_ptr *b) {
-    if ( MEnt_GlobalID(*a) > MEnt_GlobalID(*b) ) 
-      return 1;
-    else if ( MEnt_GlobalID(*a) < MEnt_GlobalID(*b) ) 
-      return -1;
-    else
-      return 0;
+
+
+  int compareINT(const void *a, const void *b) {
+    if ( *(int*)a > *(int*)b )  return 1;
+    else if ( *(int*)a < *(int*)b )  return -1;
+    else return 0;
+  }
+
+  int compareGlobalID(const void *a, const void *b) {
+     if ( MEnt_GlobalID(*(MEntity_ptr*)a) > MEnt_GlobalID(*(MEntity_ptr*)b) ) return 1 ;
+     else if ( MEnt_GlobalID(*(MEntity_ptr*)a) < MEnt_GlobalID(*(MEntity_ptr*)b) ) return -1 ;
+     else return 0;
   }
 
 
-  int compareVertexCoor(MVertex_ptr *a, MVertex_ptr *b) {
+  int compareCoorDouble(const void * a, const void * b) {
+    double tol = 1e-8;
+    int i;
+    double *coor1 = (double *)a;
+    double *coor2 = (double *)b;
+    for(i = 0; i < 3; i++) {
+      if ( (coor1[i] - coor2[i]) > tol )
+	return 1;
+      if ( (coor2[i] - coor1[i]) > tol )
+	return -1;
+    }
+    return 0;
+  }
+  
+  int compareVertexCoor(const void *a, const void *b) {
     double coor1[3], coor2[3];
     double tol = 1e-8;
     int i;
-    MV_Coords(*a,coor1);
-    MV_Coords(*b,coor2);
+    MV_Coords(*(MVertex_ptr*)a,coor1);
+    MV_Coords(*(MVertex_ptr*)b,coor2);
     for(i = 0; i < 3; i++) {
       if ( (coor1[i] - coor2[i]) > tol )
 	return 1;
