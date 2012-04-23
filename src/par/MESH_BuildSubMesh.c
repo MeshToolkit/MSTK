1bdf537544bc5bbbf2a6dda55eb8726d4c6f7f2f
duowang <devnull@localhost>
2012-04-23 12:45:44 -0600

    finish the surface mesh for BuildSubMesh()

--- src/par/MESH_BuildSubMesh.c
+++ src/par/MESH_BuildSubMesh.c
@@ -77,43 +77,56 @@ int MESH_BuildSubMesh_Face(Mesh_ptr mesh, Mesh_ptr submesh) {
   fedges = (MEdge_ptr *) malloc(MAXPV2*sizeof(MEdge_ptr));
   fedirs = (int *) malloc(MAXPV2*sizeof(int));
   while(mf = MESH_Next_Face(mesh, &idx)) {
-    printf("mf Ptype %d\n",MF_PType(mf));
     if(MF_PType(mf) != POVERLAP) continue;
     if(MEnt_IsMarked(mf,mkfid)) continue;
     List_Add(faces,mf);
     MEnt_Mark(mf,mkfid);
-    printf("hereherehere\n");
-    new_mf = MF_New(submesh);
+    new_mf = MF_New(submesh);  /* add new face and copy information */
+    MF_Set_GEntDim(new_mf,MF_GEntDim(mf));
+    MF_Set_GEntID(new_mf,MF_GEntID(mf));
+    MF_Set_PType(new_mf,MF_PType(mf));
+    MF_Set_MasterParID(new_mf,MF_MasterParID(mf));
+    MF_Set_GlobalID(new_mf,MF_GlobalID(mf));
+
     mfedges = MF_Edges(mf,1,0);
     nfe = List_Num_Entries(mfedges);
     for(j = 0; j < nfe; j++) {
       me = List_Entry(mfedges,j);
       if(MEnt_IsMarked(me,mkeid)) new_me = MESH_Edge(submesh,ME_to_list_id[ME_ID(me)]-1);
       else {
-	new_me = ME_New(submesh);
+	new_me = ME_New(submesh);    /* add new edge and copy information */
+	ME_Set_GEntDim(new_me,ME_GEntDim(me));
+	ME_Set_GEntID(new_me,ME_GEntID(me));
+	ME_Set_PType(new_me,ME_PType(me));
+	ME_Set_MasterParID(new_me,ME_MasterParID(me));
+	ME_Set_GlobalID(new_me,ME_GlobalID(me));
+
 	ME_to_list_id[ME_ID(me)] = ME_ID(new_me);
 	List_Add(edges,me);
 	MEnt_Mark(me,mkeid);
  	for(k = 0; k < 2; k++) {
 	  mv = ME_Vertex(me,k);
-	  //	  printf("mv ID: %d\n",MV_ID(mv));
 	  if(MEnt_IsMarked(mv,mkvid)) new_mv = MESH_Vertex(submesh,MV_to_list_id[MV_ID(mv)]-1);
 	  else {
-	    new_mv = MV_New(submesh);
+	    new_mv = MV_New(submesh);  /* add new vertex and copy information */
+	    MV_Set_GEntDim(new_mv,MV_GEntDim(mv));
+	    MV_Set_GEntID(new_mv,MV_GEntID(mv));
+	    MV_Set_PType(new_mv,MV_PType(mv));
+	    MV_Set_MasterParID(new_mv,MV_MasterParID(mv));
+	    MV_Set_GlobalID(new_mv,MV_GlobalID(mv));
 	    MV_Coords(mv,coor);
 	    MV_Set_Coords(new_mv,coor);
-	    //printf("new vertex %d created\n",MV_ID(new_mv));
+
 	    MV_to_list_id[MV_ID(mv)] = MV_ID(new_mv);
 	    List_Add(verts,mv);
 	    MEnt_Mark(mv,mkvid);
 	  }
 	  ME_Set_Vertex(new_me,k,new_mv);
-	  //printf("new edge %d created\n", ME_ID(new_me));
 	}
       }
       
       fedges[j] = new_me;
-      fedirs[j] = MF_EdgeDir_i(mf,j) == 1 ? 1 : -1;
+      fedirs[j] = MF_EdgeDir_i(mf,j) == 1 ? 1 : 0;
     }
     MF_Set_Edges(new_mf,nfe,fedges,fedirs);
     printf("new face %d with %d edges\n",MF_ID(new_mf),nfe);
