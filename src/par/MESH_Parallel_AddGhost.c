5ca4a464ae72701f7bb450fd31aaaf3b6c73ab2c
duowang <devnull@localhost>
2012-04-27 17:12:58 -0600

    finish the parallel add ghost function

--- src/par/MESH_Parallel_AddGhost.c
+++ src/par/MESH_Parallel_AddGhost.c
@@ -18,9 +18,9 @@ extern "C" {
   /* 
      This function is a collective call
 
-     It send the OVERLAP entities to neighbor processors
-
-     Assume OVERLAP list is properly built 
+     It send the OVERLAP elements to neighbor processors
+     It uses SendMesh and RecvMesh to communicate
+     Assume OVERLAP elements is properly labelled
 
      Author(s): Duo Wang, Rao Garimella
   */
@@ -54,13 +54,8 @@ int MESH_Parallel_AddGhost(Mesh_ptr submesh, int rank, int num,  MPI_Comm comm) 
      First update the parallel adjancy information, 
   */
 int MESH_Parallel_AddGhost_Face(Mesh_ptr submesh, int rank, int num, MPI_Comm comm) {
-  RepType rtype;
-  int i, j, num_recv_procs, index_recv_mesh;
-  MVertex_ptr mv;
-  MEdge_ptr me;
-  MFace_ptr mf;
-  List_ptr fedges, fverts;
-  int adj, idx;
+  int i, num_recv_procs, index_recv_mesh;
+  int adj;
   Mesh_ptr send_mesh;
   Mesh_ptr *recv_meshes;
 
@@ -70,19 +65,17 @@ int MESH_Parallel_AddGhost_Face(Mesh_ptr submesh, int rank, int num, MPI_Comm co
 
   /* 
      first update parallel adjancy information
-     any two processor that has vertex connection now has face connection
+     any two processor that has vertex connection now has all connection
   */
   for (i = 0; i < num; i++) {
     if(i == rank) continue;
-    if( adj = MESH_Has_Ghosts_From_Prtn(submesh,i,MANYTYPE) ) {
-      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MFACE);
-      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MVERTEX);
-      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MFACE);
+    if( adj = MESH_Has_Ghosts_From_Prtn(submesh,i,MVERTEX) ) {
+      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MALLTYPE);
+      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MALLTYPE);
     }
-    if( adj = MESH_Has_Overlaps_On_Prtn(submesh,i,MANYTYPE) ) {
-      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MFACE);
-      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MVERTEX);
-      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MFACE);
+    if( adj = MESH_Has_Overlaps_On_Prtn(submesh,i,MVERTEX) ) {
+      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MALLTYPE);
+      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MALLTYPE);
     }
   }
   /* allocate meshes to receive from other processors */
@@ -91,7 +84,7 @@ int MESH_Parallel_AddGhost_Face(Mesh_ptr submesh, int rank, int num, MPI_Comm co
   for(i = 0; i < num_recv_procs; i++)
     recv_meshes[i] = MESH_New(MESH_RepType(submesh));
 
-  printf(" number of recv_procs %d,on rank %d\n",rank, num_recv_procs);
+  printf(" number of recv_procs %d,on rank %d\n", num_recv_procs, rank);
 
   /*
   for(i = 0; i < num; i++) {
@@ -163,7 +156,10 @@ int MESH_Parallel_AddGhost_Face(Mesh_ptr submesh, int rank, int num, MPI_Comm co
   }
   }
   */
-
+  /*
+  for (i = 0; i < num_recv_procs; i++) 
+     MESH_CheckTopo(recv_meshes[i]);
+  */
   MESH_ConcatSubMesh(submesh, num_recv_procs, recv_meshes);
   //for (i = 0; i < num; i++) 
   //  MESH_Delete(recv_meshes[i]);
@@ -175,43 +171,121 @@ int MESH_Parallel_AddGhost_Face(Mesh_ptr submesh, int rank, int num, MPI_Comm co
   /* right now assume there are no overlapped regions */
 
 int MESH_Parallel_AddGhost_Region(Mesh_ptr submesh, int rank, int num, MPI_Comm comm) {
-  int i, nv, ne, nf, nr, global_id, mesh_info[10];
-  MRegion_ptr mr;
-  RepType rtype;
-  int *global_mesh_info;
+  int i, num_recv_procs, index_recv_mesh;
+  int adj;
+  Mesh_ptr send_mesh;
+  Mesh_ptr *recv_meshes;
 
+  /* build the 1-ring outside layer send mesh */
+  send_mesh = MESH_New(MESH_RepType(submesh));
+  MESH_BuildSubMesh(submesh,send_mesh);
 
-  for (i = 0; i < 10; i++) mesh_info[i] = 0;
+  /* 
+     first update parallel adjancy information
+     any two processor that has vertex connection now has all connection
+  */
+  for (i = 0; i < num; i++) {
+    if(i == rank) continue;
+    if( adj = MESH_Has_Ghosts_From_Prtn(submesh,i,MVERTEX) ) {
+      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MALLTYPE);
+      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MALLTYPE);
 
-  /* mesh_info store the mesh reptype, nv, ne, nf, nbf */
-  rtype = MESH_RepType(submesh);
-  nv = MESH_Num_Vertices(submesh);
-  ne = MESH_Num_Edges(submesh);
-  nf = MESH_Num_Faces(submesh);
-  nr = MESH_Num_Regions(submesh);
+    }
+    if( adj = MESH_Has_Overlaps_On_Prtn(submesh,i,MVERTEX) ) {
+      MESH_Flag_Has_Overlaps_On_Prtn(submesh,i,MALLTYPE);
+      MESH_Flag_Has_Ghosts_From_Prtn(submesh,i,MALLTYPE);
+    }
+  }
+  /* allocate meshes to receive from other processors */
+  num_recv_procs = MESH_Num_GhostPrtns(submesh);
+  recv_meshes = (Mesh_ptr*)MSTK_malloc(num_recv_procs*sizeof(Mesh_ptr));
+  for(i = 0; i < num_recv_procs; i++)
+    recv_meshes[i] = MESH_New(MESH_RepType(submesh));
 
-  mesh_info[0] = rtype;
-  mesh_info[1] = nv;
-  mesh_info[2] = ne;
-  mesh_info[3] = nf;
-  mesh_info[4] = nr;
+  printf(" 3D number of recv_procs %d,on rank %d\n", num_recv_procs, rank);
 
-  global_mesh_info = (int *)MSTK_malloc(10*num*sizeof(int));
-  MPI_Allgather(mesh_info,10,MPI_INT,global_mesh_info,10,MPI_INT,comm);
-
-  /* calculate starting global id number for faces*/
-  global_id = 1;
-  for(i = 0; i < rank; i++) 
-    global_id = global_id + global_mesh_info[10*i+4];
-  for(i = 0; i < nf; i++) {
-    mr = MESH_Region(submesh,i);
-    MR_Set_PType(mr,PINTERIOR);
-    MR_Set_GlobalID(mr,global_id++);
-    MR_Set_MasterParID(mr,rank);
+  index_recv_mesh = 0;
+  for (i = 0; i < num; i++) {
+    if(i == rank) continue;
+    if(i < rank) {     
+      if( MESH_Has_Ghosts_From_Prtn(submesh,i,MREGION) ) {
+	MESH_RecvMesh(recv_meshes[index_recv_mesh++],3,i,rank,comm);
+	//printf("rank %d receives elements from rank %d\n", rank,i);
+      }
+      if( MESH_Has_Overlaps_On_Prtn(submesh,i,MREGION) ) {
+	MESH_SendMesh(send_mesh,i,comm);
+	//printf("rank %d sends elements to rank %d\n", rank,i);
+      }
+    }
+    if(i > rank) {     
+      if( MESH_Has_Overlaps_On_Prtn(submesh,i,MREGION) ) {
+	MESH_SendMesh(send_mesh,i,comm);
+	//printf("rank %d sends elements to rank %d\n", rank,i);
+      }
+      if( MESH_Has_Ghosts_From_Prtn(submesh,i,MREGION) ) {
+	MESH_RecvMesh(recv_meshes[index_recv_mesh++],3,i,rank,comm);
+	//printf("rank %d receives elements from rank %d\n", rank,i);
+      }
+    }
   }
 
-  MSTK_free(global_mesh_info);
+  /*
+  Mesh_ptr mymesh = recv_meshes[0];
+  int nv, ne, nf, nr;
+  MVertex_ptr mv;
+  MEdge_ptr me;
+  MFace_ptr mf;
+  MRegion_ptr mr;
+  nv = MESH_Num_Vertices(mymesh);
+  ne = MESH_Num_Edges(mymesh);
+  nf = MESH_Num_Faces(mymesh);
+  nr = MESH_Num_Regions(mymesh);
+  double coor[3];
+  if(rank == 0) {
+    for(i = 0; i < nv; i++) {
+      mv = MESH_Vertex(mymesh,i);
+      MV_Coords(mv,coor);
+      printf("Concat rank %d, local id %d, global %d master id %d ",rank,MV_ID(mv),MV_GlobalID(mv), MV_MasterParID(mv));
+      printf("PType: %d, coordinate: (%lf, %lf, %lf)\n",MV_PType(mv),coor[0],coor[1],coor[2]);
+    }
+    
+    for(i = 0; i < ne; i++) {
+      me = MESH_Edge(mymesh,i);
+      printf("Edge: on rank %d, local id %d, global id %d, PType: %d, Master Partition: %d vertex 1: %d, vertex 2: %d\n",rank,ME_ID(me),ME_GlobalID(me), ME_PType(me), ME_MasterParID(me), MV_ID(ME_Vertex(me,0)),MV_ID(ME_Vertex(me,1)));
+    }
+    
+  int j, dir;
+  for(i = 0; i < nf; i++) {
+    mf = MESH_Face(mymesh,i);
+    printf("Face: on rank %d, local id %d, global id %d, PType: %d, Master Partition: %d\n",rank,MF_ID(mf),MF_GlobalID(mf), MF_PType(mf), MF_MasterParID(mf) );
+    for(j = 0; j < List_Num_Entries(MF_Edges(mf,1,0)); j++) {
+      dir = MF_EdgeDir_i(mf,j) == 1 ? 1 : -1;
+      printf("dir: %d, eid: %d\n", dir, dir*ME_ID(List_Entry(MF_Edges(mf,1,0),j)));
+    }
+    printf("\n");
+  }
+  for(i = 0; i < nr; i++) {
+    mr = MESH_Region(mymesh,i);
+    printf("Region: on rank %d, local id %d, global id %d, PType: %d, Master Partition: %d\n",rank,MR_ID(mr),MR_GlobalID(mr), MR_PType(mr), MR_MasterParID(mr) );
+    for(j = 0; j < List_Num_Entries(MR_Faces(mr)); j++) {
+      dir = MR_FaceDir_i(mr,j) == 1 ? 1 : -1;
+      printf("dir: %d, eid: %d\n", dir, dir*ME_ID(List_Entry(MR_Faces(mr),j)));
+    }
+    printf("\n");
+  }
+  }
+  */
+  /*
+  for (i = 0; i < num_recv_procs; i++) 
+     MESH_CheckTopo(recv_meshes[i]);
+  */
+    MESH_ConcatSubMesh(submesh, num_recv_procs, recv_meshes);
+  //for (i = 0; i < num; i++) 
+  //  MESH_Delete(recv_meshes[i]);
+  //  printf("after  rank %d, ov_mesh num of vertex %d, number of face %d\n",rank,MESH_Num_Vertices(submesh),MESH_Num_Faces(submesh));
+
   return 1;
+
 }
 
 #ifdef __cplusplus
