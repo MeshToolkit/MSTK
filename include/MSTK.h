975bfd69c5e9e1c5d0a298aa2ad043aa608d8039
Rao Garimella <rao@lanl.gov>
2012-06-26 12:56:30 -0600

    Added mrtype to region definition and added MR_ElementType and MF_ElementType calls

--- include/MSTK.h
+++ include/MSTK.h
@@ -285,6 +285,8 @@ void MSTK_Init(void);
   int MF_GEntDim(MFace_ptr mface);
   int MF_GEntID(MFace_ptr mface);
   GEntity_ptr MF_GEntity(MFace_ptr mface);
+
+  MFType MF_ElementType(MFace_ptr mf);
   int MF_Num_Vertices(MFace_ptr mface);
   int MF_Num_Edges(MFace_ptr mface);
   int MF_Num_AdjFaces(MFace_ptr mface);
@@ -364,6 +366,7 @@ void MSTK_Init(void);
   int MR_GEntID(MRegion_ptr mregion);
   GEntity_ptr MR_GEntity(MRegion_ptr mregion);
 
+  MRType MR_ElementType(MRegion_ptr mregion);
   int MR_Num_Vertices(MRegion_ptr mregion);
   int MR_Num_Edges(MRegion_ptr mregion);
   int MR_Num_Faces(MRegion_ptr mregion);
