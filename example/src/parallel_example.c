939fe1cddd32866e0393a4dccdfd6b89540d2f85
Rao Garimella <rao@lanl.gov>
2014-12-08 14:04:09 -0700

    Replace call to MSTK_UpdateAttr with MESH_UpdateAttributes

--- example/src/parallel_example.c
+++ example/src/parallel_example.c
@@ -120,7 +120,7 @@ int main(int argc, char *argv[]) {
   /* Update attributes across processors */
   
   fprintf(stderr,"Updating attributes on proc %-d...",rank);
-  MSTK_UpdateAttr(mymesh,comm);
+  MESH_UpdateAttributes(mymesh,comm);
   fprintf(stderr,"done\n");
 
 
@@ -203,7 +203,7 @@ int main(int argc, char *argv[]) {
     fprintf(stderr,"done\n");
 
     fprintf(stderr,"Updating attributes on proc %-d...",rank);
-    MSTK_UpdateAttr(mymesh,comm);
+    MESH_UpdateAttributes(mymesh,comm);
     fprintf(stderr,"done\n");
 
     fprintf(fp,"\n\n");
