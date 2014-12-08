03c4a521a7899e6ab0582c731b02b94ac0ebeb55
Rao Garimella <rao@lanl.gov>
2014-12-08 12:55:24 -0700

    Recv vertices and their basic info (but no coords)

---
+++ src/par/MESH_Recv_Vertices.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "MSTK.h"
+#include "MSTK_private.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /* 
+     This function receives mesh vertices from processor 'fromrank'
+
+     Author(s): Rao Garimella
+  */
+
+
+  int MESH_Recv_Vertices(Mesh_ptr mesh, int fromrank, int nvertices, 
+                         MSTK_Comm comm) {
+    int i;
+    MVertex_ptr v;
+    MPI_Status status;
+    MPI_Request request;
+    char mesg[256], errorstr[256], funcname[256]="MESH_Recv_Vertices";
+    int errcode, len, nreq=0;
+
+    int rank;
+    MPI_Comm_rank(comm,&rank);
+
+    /* allocate receive buffer */
+    int *list_vertex = (int *) malloc(3*nvertices*sizeof(int));
+
+    /* receive vertex info */
+    errcode = MPI_Irecv(list_vertex,3*nvertices,MPI_INT,fromrank,rank,comm,
+                        &request);
+    if (errcode != MPI_SUCCESS)
+      MSTK_Report(funcname,"Trouble receiving mesh vertex info",MSTK_FATAL);
+    
+
+    /* Create the vertices while waiting for the message to complete */
+
+    for (i = 0; i < nvertices; i++)
+      v = MV_New(mesh);
+
+
+    errcode = MPI_Wait(&request,MPI_STATUS_IGNORE);
+    if (errcode != MPI_SUCCESS)
+      MSTK_Report(funcname,"Trouble receiving mesh vertex info",MSTK_FATAL);    
+
+    for(i = 0; i < nvertices; i++) {
+      v = MESH_Vertex(mesh,i);
+      MV_Set_GEntDim(v,(list_vertex[3*i] & 7));
+      MV_Set_GEntID(v,(list_vertex[3*i] >> 3));
+      MV_Set_PType(v,(list_vertex[3*i+1] & 3));
+      MV_Set_MasterParID(v,(list_vertex[3*i+1] >> 2));
+      MV_Set_GlobalID(v,list_vertex[3*i+2]);
+    }
+
+    free(list_vertex);    
+
+    return 1;
+  }
+
+
+#ifdef __cplusplus
+}
+#endif
+
