f55ebd5dc0a833f3a4acc6583129a7465fee4395
Rao Garimella <rao@lanl.gov>
2016-01-19 09:16:15 -0700

    Added tests for gather-scatter operation on attributes and also for exchanging attributes on partition boundaries

--- unittests/parallel/4proc/Test_UpdAtt.cc
+++ unittests/parallel/4proc/Test_UpdAtt.cc
@@ -1,6 +1,7 @@
 #include <UnitTest++.h>
 
 #include "../../../include/MSTK.h"
+#include "../../../include/MSTK_private.h"
 
 SUITE(Parallel) {
 TEST(UpdAtt2D_Dist) {
@@ -193,6 +194,431 @@ TEST(UpdAtt2D_Weave) {
   return;
 
 }
+
+
+
+TEST(GathScatAttrib_2D) {
+
+  int i, idx, ival, status;
+  int vcol, fcol, mpid;
+  int nproc, rank, dim;
+  double rval;
+  void *pval;
+  Mesh_ptr mesh;
+  char filename[256];
+
+
+  MSTK_Init();
+
+  MSTK_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_size(comm,&nproc);
+  MPI_Comm_rank(comm,&rank);
+
+  Mesh_ptr mesh0;
+  if (rank == 0) {
+    mesh0 = MESH_New(UNKNOWN_REP);
+
+    sprintf(filename,"parallel/4proc/quad10x10.mstk");
+    MESH_InitFromFile(mesh0, filename, comm);
+    
+    if (MESH_Num_Regions(mesh0) > 0)
+      fprintf(stderr,"Code is for surface meshes only. Exiting...\n");
+    else if (MESH_Num_Faces(mesh0) > 0)
+      dim = 2;
+    else {
+      fprintf(stderr,"Mesh is neither solid nor surface mesh. Exiting...\n");
+      exit(-1);
+    }
+
+    status = MESH_CheckTopo(mesh0);
+    CHECK(status);
+  }
+
+//  DebugWait=1;
+//  while (DebugWait);
+
+  int ring = 1; /* One ring ghosts */
+  int with_attr = 1; /* Do allow exchange of attributes */
+  int del_inmesh = 1; /* delete input mesh after partitioning */
+  int method;
+
+#if defined (_MSTK_HAVE_METIS)
+  method = 0;
+#elif defined (_MSTK_HAVE_ZOLTAN)
+  method = 1;
+#else
+  fprintf(stderr,"Cannot find partitioner\n");
+  status = 0;
+  CHECK(status);
+#endif
+
+  mesh = NULL;
+  MSTK_Mesh_Distribute(mesh0, &mesh, &dim, ring, with_attr, method, 
+		       del_inmesh, comm);
+
+  status = MESH_CheckTopo(mesh);
+  CHECK(status);
+
+
+
+  MAttrib_ptr vertatt, edgeatt;
+  vertatt = MAttrib_New(mesh,"vertatt",DOUBLE,MVERTEX);
+  edgeatt = MAttrib_New(mesh,"edgeatt",DOUBLE,MEDGE);
+
+
+  for (int i = 1; i < 5; i++) {
+    MAttOpType op = (MAttOpType) i;
+
+    MVertex_ptr mv;
+    idx = 0;
+    while ((mv = MESH_Next_Vertex(mesh,&idx)))
+      MEnt_Set_AttVal(mv,vertatt,0,rank+1,NULL);
+
+    MESH_GathScat1Attribute(mesh,vertatt,op,comm);
+
+
+    /* Now use plain old MPI to determine the right answer */
+
+    int nv = MESH_Num_Vertices(mesh);
+    int *allnv = (int *) malloc(nproc*sizeof(int));
+    MPI_Allgather(&nv,1,MPI_INT,allnv,1,MPI_INT,MPI_COMM_WORLD);
+
+    int maxnv = 0;
+    for (int j = 0; j < nproc; j++)
+      if (allnv[j] > maxnv)
+        maxnv = allnv[j];
+    int *locgidlist = (int *) calloc(maxnv,sizeof(int));
+    double *locvallist = (double *) calloc(maxnv,sizeof(double));
+    int *globgidlist = (int *) calloc(maxnv,nproc*sizeof(int));
+    double *globvallist = (double *) calloc(maxnv,nproc*sizeof(double));
+
+    int j = 0;
+    idx = 0; 
+    while ((mv = MESH_Next_Vertex(mesh,&idx))) {
+      locgidlist[j] = MV_GlobalID(mv);
+      locvallist[j] = rank+1;
+      j++;
+    }
+
+    MPI_Allgather(locgidlist,maxnv,MPI_INT,globgidlist,maxnv,MPI_INT,MPI_COMM_WORLD);
+    MPI_Allgather(locvallist,maxnv,MPI_DOUBLE,globvallist,maxnv,MPI_DOUBLE,MPI_COMM_WORLD);
+
+    idx = 0;
+    while ((mv = MESH_Next_Vertex(mesh,&idx))) {
+      int idx;
+      
+      int ival;
+      double rval;
+      void *pval;
+
+      MEnt_Get_AttVal(mv,vertatt,&ival,&rval,&pval);
+
+      int gid = MV_GlobalID(mv);
+
+      switch (op) {
+        case ATTOP_MAX: {
+          double maxval = -1;
+          for (int k = 0; k < maxnv*nproc; k++) 
+            if (globgidlist[k] == gid)
+              if (globvallist[k] > maxval)
+                maxval = globvallist[k];
+          CHECK_EQUAL(maxval,rval);
+          break;
+        }
+        case ATTOP_MIN: {
+          double minval = 1e+16;
+          idx = 0;
+          for (int k = 0; k < maxnv*nproc; k++) 
+            if (globgidlist[k] == gid)
+              if (globvallist[k] < minval)
+                minval = globvallist[k];          
+          CHECK_EQUAL(minval,rval);
+          break;
+        }
+        case ATTOP_SUM: case ATTOP_AVG: {
+          double sum = 0.0;
+          int nval = 0;
+          idx = 0;
+          for (int k = 0; k < maxnv*nproc; k++) 
+            if (globgidlist[k] == gid) {
+              sum += globvallist[k];
+              nval++;
+            }
+          if (op == ATTOP_SUM)
+            CHECK_EQUAL(sum,rval);
+          else
+            CHECK_CLOSE(sum/nval,rval,1.0e-12);
+        }
+        default: 
+          break;
+      }
+    } /* for each vertex */
+    free(locgidlist);
+    free(locvallist);
+    free(globgidlist);
+    free(globvallist);
+
+
+
+
+
+    MEdge_ptr me;
+    idx = 0;
+    while (me = MESH_Next_Edge(mesh,&idx))
+      MEnt_Set_AttVal(me,edgeatt,0,rank+1,NULL);
+
+    MESH_GathScat1Attribute(mesh,edgeatt,op,comm);
+
+
+    /* Now use plain old MPI to determine the right answer */
+
+    int ne = MESH_Num_Edges(mesh);
+    int *allne = (int *) malloc(nproc*sizeof(int));
+    MPI_Allgather(&ne,1,MPI_INT,allne,1,MPI_INT,MPI_COMM_WORLD);
+
+    int maxne = 0;
+    for (int j = 0; j < nproc; j++)
+      if (allne[j] > maxne)
+        maxne = allne[j];
+    locgidlist = (int *) calloc(maxne,sizeof(int));
+    locvallist = (double *) calloc(maxne,sizeof(double));
+    globgidlist = (int *) calloc(maxne,nproc*sizeof(int));
+    globvallist = (double *) calloc(maxne,nproc*sizeof(double));
+
+    j = 0;
+    idx = 0; 
+    while ((me = MESH_Next_Edge(mesh,&idx))) {
+      locgidlist[j] = ME_GlobalID(me);
+      locvallist[j] = rank+1;
+      j++;
+    }
+
+    MPI_Allgather(locgidlist,maxne,MPI_INT,globgidlist,maxne,MPI_INT,MPI_COMM_WORLD);
+    MPI_Allgather(locvallist,maxne,MPI_DOUBLE,globvallist,maxne,MPI_DOUBLE,MPI_COMM_WORLD);
+
+    idx = 0;
+    while ((me = MESH_Next_Edge(mesh,&idx))) {
+      int idx;
+      
+      int ival;
+      double rval;
+      void *pval;
+
+      MEnt_Get_AttVal(me,edgeatt,&ival,&rval,&pval);
+
+      int gid = ME_GlobalID(me);
+
+      switch (op) {
+        case ATTOP_MAX: {
+          double maxval = -1;
+          for (int k = 0; k < maxne*nproc; k++) 
+            if (globgidlist[k] == gid)
+              if (globvallist[k] > maxval)
+                maxval = globvallist[k];
+          CHECK_EQUAL(maxval,rval);
+          break;
+        }
+        case ATTOP_MIN: {
+          double minval = 1e+16;
+          idx = 0;
+          for (int k = 0; k < maxne*nproc; k++) 
+            if (globgidlist[k] == gid)
+              if (globvallist[k] < minval)
+                minval = globvallist[k];          
+          CHECK_EQUAL(minval,rval);
+          break;
+        }
+        case ATTOP_SUM: case ATTOP_AVG: {
+          double sum = 0.0;
+          int nval = 0;
+          idx = 0;
+          for (int k = 0; k < maxne*nproc; k++) 
+            if (globgidlist[k] == gid) {
+              sum += globvallist[k];
+              nval++;
+            }
+          if (op == ATTOP_SUM)
+            CHECK_EQUAL(sum,rval);
+          else
+            CHECK_CLOSE(sum/nval,rval,1.0e-12);
+        }
+        default: 
+          break;
+      }
+    } /* for each edge */
+    free(locgidlist);
+    free(locvallist);
+    free(globgidlist);
+    free(globvallist);
+
+
+  } /* For each GathScat operation type */
+
+  return;
+} /* GathScatAttrib_2D */
+ 
+
+
+
+TEST(XchngEdgeAttrib_2D) {
+
+  int i, idx, ival, status;
+  int vcol, fcol, mpid;
+  int nproc, rank, dim;
+  double rval;
+  void *pval;
+  Mesh_ptr mesh;
+  char filename[256];
+
+
+  MSTK_Init();
+
+  MSTK_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_size(comm,&nproc);
+  MPI_Comm_rank(comm,&rank);
+
+  Mesh_ptr mesh0;
+  if (rank == 0) {
+    mesh0 = MESH_New(UNKNOWN_REP);
+
+    sprintf(filename,"parallel/4proc/quad10x10.mstk");
+    MESH_InitFromFile(mesh0, filename, comm);
+    
+    if (MESH_Num_Regions(mesh0) > 0)
+      fprintf(stderr,"Code is for surface meshes only. Exiting...\n");
+    else if (MESH_Num_Faces(mesh0) > 0)
+      dim = 2;
+    else {
+      fprintf(stderr,"Mesh is neither solid nor surface mesh. Exiting...\n");
+      exit(-1);
+    }
+
+    status = MESH_CheckTopo(mesh0);
+    CHECK(status);
+  }
+
+  int ring = 1; /* One ring ghosts */
+  int with_attr = 1; /* Do allow exchange of attributes */
+  int del_inmesh = 1; /* delete input mesh after partitioning */
+  int method;
+
+#if defined (_MSTK_HAVE_METIS)
+  method = 0;
+#elif defined (_MSTK_HAVE_ZOLTAN)
+  method = 1;
+#else
+  fprintf(stderr,"Cannot find partitioner\n");
+  status = 0;
+  CHECK(status);
+#endif
+
+  mesh = NULL;
+  MSTK_Mesh_Distribute(mesh0, &mesh, &dim, ring, with_attr, method, 
+		       del_inmesh, comm);
+
+  status = MESH_CheckTopo(mesh);
+  CHECK(status);
+
+  MAttrib_ptr edgeatt;
+  edgeatt = MAttrib_New(mesh,"edgeatt",DOUBLE,MEDGE);
+
+  MEdge_ptr me;
+  idx = 0;
+  while (me = MESH_Next_Edge(mesh,&idx))
+    MEnt_Set_AttVal(me,edgeatt,0,rank+1,NULL);
+  
+  MESH_XchngEdgeFaceAttrib(mesh,edgeatt,comm);
+
+  /* Mark/count all edges on the partition boundary */
+
+  int prtnmark = MSTK_GetMarker();
+
+  int ne = 0;
+  idx = 0; 
+  while ((me = MESH_Next_Edge(mesh,&idx))) {
+    int foundowned=0, foundghost=0;
+    List_ptr efaces = ME_Faces(me);
+    MFace_ptr vf;
+    int idx2 = 0;
+    while ((vf = List_Next_Entry(efaces,&idx2))) {
+      if (MF_PType(vf) == PGHOST)
+        foundghost = 1;
+      else
+        foundowned = 1;
+    }
+    if (!foundghost || !foundowned) continue;
+
+    MEnt_Mark(me,prtnmark);
+    ne++;
+  }
+
+  /* Now use plain old MPI to determine the right answer */
+  
+  int *allne = (int *) malloc(nproc*sizeof(int));
+  MPI_Allgather(&ne,1,MPI_INT,allne,1,MPI_INT,MPI_COMM_WORLD);
+  
+  int maxne = 0;
+  for (int j = 0; j < nproc; j++)
+    if (allne[j] > maxne)
+      maxne = allne[j];
+  int *locgidlist = (int *) calloc(maxne,sizeof(int));
+  double *locvallist = (double *) calloc(maxne,sizeof(double));
+  int *globgidlist = (int *) calloc(maxne,nproc*sizeof(int));
+  double *globvallist = (double *) calloc(maxne,nproc*sizeof(double));  
+
+  int j = 0;
+  idx = 0; 
+  while ((me = MESH_Next_Edge(mesh,&idx))) {
+    if (!MEnt_IsMarked(me, prtnmark)) continue;
+    locgidlist[j] = ME_GlobalID(me);
+    locvallist[j] = rank+1;
+    j++;
+  }
+  
+  MPI_Allgather(locgidlist,maxne,MPI_INT,globgidlist,maxne,MPI_INT,MPI_COMM_WORLD);
+  MPI_Allgather(locvallist,maxne,MPI_DOUBLE,globvallist,maxne,MPI_DOUBLE,MPI_COMM_WORLD);
+  
+  idx = 0;
+  while ((me = MESH_Next_Edge(mesh,&idx))) { 
+    if (!MEnt_IsMarked(me,prtnmark)) continue;
+
+    int gid = ME_GlobalID(me);
+    
+    int ival;
+    double rval;
+    void *pval;
+    
+    MEnt_Get_AttVal(me,edgeatt,&ival,&rval,&pval);
+    
+    double rval_mpi=-1e+10;
+    int found = 0;
+    for (int p = 0; p < nproc; p++) {
+      if (p == rank) continue;
+      for (int k = 0; k < maxne; k++) 
+        if (globgidlist[p*maxne+k] == gid) {
+          rval_mpi = globvallist[p*maxne+k];
+          found = 1;
+          break;
+        }
+      if (found) break;
+    }
+    CHECK_EQUAL(rval_mpi,rval);
+  } /* for each edge */
+
+  free(locgidlist);
+  free(locvallist);
+  free(globgidlist);
+  free(globvallist);
+
+  idx = 0;
+  while ((me = MESH_Next_Edge(mesh,&idx)))
+    MEnt_Unmark(me,prtnmark);
+
+  MSTK_FreeMarker(prtnmark);
+    
+  return;
+} /* XchngEdgeAttrib_2D */
+ 
 
 }
 
