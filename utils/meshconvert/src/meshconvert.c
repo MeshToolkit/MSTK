a80dedeb7b179f8dc274c8897304ca31a0202ad0
Rao Garimella <rao@abyzou.lanl.gov>
2019-02-28 14:48:01 -0700

    restructured to allow for greater flexibility

--- utils/meshconvert/src/meshconvert.c
+++ utils/meshconvert/src/meshconvert.c
@@ -8,16 +8,44 @@
 #include <mpi.h>
 #endif
 
-#include "MSTK.h" 
+#include "MSTK.h"
+
+MshFmt getFormat(char *filename) {
+  int len = strlen(filename);
+  if (len > 5 && strncmp(&(filename[len-5]),".mstk",5) == 0)
+    return MSTK;
+  else if (len > 4 && strncmp(&(filename[len-4]),".gmv",4) == 0)
+    return GMV;
+  else if ((len > 4 && strncmp(&(filename[len-4]),".exo",4) == 0) ||
+	   (len > 2 && strncmp(&(filename[len-2]),".g",2) == 0) ||
+	   (len > 2 && strncmp(&(filename[len-2]),".e",2) == 0))
+    return EXODUSII;
+  else if (len > 4 && strncmp(&(filename[len-4]),".par",4) == 0) 
+    return NEMESISI;
+  else if ((len > 4 && strncmp(&(filename[len-4]),".avs",4) == 0) ||
+	   (len > 4 && strncmp(&(filename[len-4]),".inp",4) == 0))
+    return AVSUCD;
+  else if (len > 4 && strncmp(&(filename[len-4]),".x3d",4) == 0)
+    return X3D;
+  else if (len > 4 && strncmp(&(filename[len-4]),".vtk",4) == 0)
+    return VTK;
+  else if (len > 5 && strncmp(&(filename[len-5]),".cgns",5) == 0)
+    return CGNS;
+  else {
+    fprintf(stderr,"Unrecognized mesh format\n");
+    fprintf(stderr,"Recognized mesh formats: MSTK, GMV, ExodusII, NemesisI, AVS/UCD, VTK, CGNS\n");
+    exit(-1);
+  }
+}
 
 int main(int argc, char *argv[]) {
   char infname[256], outfname[256];
-  Mesh_ptr mesh;
   int len, ok;
-  int build_classfn=1, partition=0, weave=0, use_geometry=0, parallel_check=0;
+  int build_classfn=1, partition=-1, weave=-1, use_geometry=0, parallel_check=0;
   int check_topo=0;
   int num_ghost_layers=0, partmethod=0;
-  MshFmt inmeshformat, outmeshformat;
+  MshFmt inmeshfmt, outmeshfmt;
+  FILE *fp;
 
   if (argc < 3) {
     fprintf(stderr,"\n");
@@ -90,10 +118,13 @@ int main(int argc, char *argv[]) {
       else if (strncmp(argv[i],"--partition=",12) == 0) {
         if (strncmp(argv[i]+12,"y",1) == 0 ||
             strncmp(argv[i]+12,"1",1) == 0)
-          partition = 1;
+          partition = 1;  // new method for partitioning serial file
+        else if (strncmp(argv[i]+12,"o",1) == 0 ||
+                 strncmp(argv[i]+12,"2",1) == 0) 
+          partition = 2;  // old method for partitioning serial file
         else if (strncmp(argv[i]+12,"n",1) == 0 ||
                  strncmp(argv[i]+12,"0",1) == 0) 
-          partition = 0;
+          partition = 0;  // no partitioning
         else
           MSTK_Report("meshconvert",
                       "--partition option should be y, n, 1 or 0",
@@ -137,283 +168,309 @@ int main(int argc, char *argv[]) {
       else
         fprintf(stderr,"Unrecognized option...Ignoring\n");
     }
-
-    /* If running on multiple processors, assume that you either partition
-       a serial mesh or weave together distributed meshes */
-
-    if (numprocs > 1 && weave == 0 && partition == 0) 
-      partition = 1; 
   }
 
+  /* what format is the input mesh file in? */
   strcpy(infname,argv[argc-2]);
+  inmeshfmt = getFormat(infname);
+  if (inmeshfmt == EXODUSII && weave == 1) {
+    inmeshfmt = NEMESISI;
+  }
+
+  /* what format should the output mesh file be in? */
   strcpy(outfname,argv[argc-1]);
+  outmeshfmt = getFormat(outfname);
 
 
-  len = strlen(infname);
-  if (len > 5 && strncmp(&(infname[len-5]),".mstk",5) == 0) {
-    inmeshformat = MSTK;
-    if (argc > 3 && strncmp(argv[1],"--classify=y",12) == 0)
-      build_classfn = 1;
-    else
-      build_classfn = 0;
+
+  /* Do we have single or multiple input files? */
+  int serial_file = 0, parallel_file = 0;
+  if (fp = fopen(infname,"r")) {
+    serial_file = 1;
+    fclose(fp);
   }
-  else if (len > 4 && strncmp(&(infname[len-4]),".gmv",4) == 0)
-    inmeshformat = GMV;
-  else if ((len > 4 && strncmp(&(infname[len-4]),".exo",4) == 0) ||
-	   (len > 2 && strncmp(&(infname[len-2]),".g",2) == 0) ||
-	   (len > 2 && strncmp(&(infname[len-2]),".e",2) == 0)) {
-    if (weave)
-      inmeshformat = NEMESISI; /* clearly user wants weave distributed meshes */
-    else
-      inmeshformat = EXODUSII;
-  }
-  else if (len > 4 && strncmp(&(infname[len-4]),".par",4) == 0) 
-    inmeshformat = NEMESISI;
-  else if ((len > 4 && strncmp(&(infname[len-4]),".avs",4) == 0) ||
-	   (len > 4 && strncmp(&(infname[len-4]),".inp",4) == 0))
-    inmeshformat = AVSUCD;
-  else if (len > 4 && strncmp(&(infname[len-4]),".x3d",4) == 0)
-    inmeshformat = X3D;
-  else {
-    fprintf(stderr,"Unrecognized input mesh format\n");
-    fprintf(stderr,"Recognized input mesh formats: MSTK, GMV, ExodusII, NemesisI, AVS/UCD\n");
-    exit(-1);
-  }
-
-  len = strlen(outfname);
-  if (len > 5 && strncmp(&(outfname[len-5]),".mstk",5) == 0)
-    outmeshformat = MSTK;
-  else if (len > 4 && strncmp(&(outfname[len-4]),".gmv",4) == 0)
-    outmeshformat = GMV;
-  else if ((len > 4 && strncmp(&(outfname[len-4]),".exo",4) == 0) ||
-	   (len > 2 && strncmp(&(outfname[len-2]),".g",2) == 0) ||
-	   (len > 2 && strncmp(&(outfname[len-2]),".e",2) == 0))
-    outmeshformat = EXODUSII;
-  else if ((len > 4 && strncmp(&(outfname[len-4]),".par",4) == 0))
-    outmeshformat = NEMESISI;
-  else if (len > 4 && strncmp(&(outfname[len-4]),".vtk",4) == 0)
-    outmeshformat = VTK;
-  else if (len > 5 && strncmp(&(outfname[len-5]),".cgns",5) == 0)
-    outmeshformat = CGNS;
-  else if ((len > 4 && strncmp(&(outfname[len-4]),".avs",4) == 0) ||
-	   (len > 4 && strncmp(&(outfname[len-4]),".inp",4) == 0))
-    outmeshformat = AVSUCD;
-  else if (len > 4 && strncmp(&(outfname[len-4]),".x3d",4) == 0)
-    outmeshformat = X3D;
-  else if (len > 3 && strncmp(&(outfname[len-3]),".dx",3) == 0)
-    outmeshformat = DX;
-  else {
-    fprintf(stderr,"Unrecognized output mesh format\n");
-    fprintf(stderr,"Recognized output mesh formats: MSTK, GMV, ExodusII, NemesisI, CGNS, VTK, AVS/UCD\n");
-    exit(-1);
-  }
-
-
-  if (inmeshformat == MSTK) {
-    mesh = MESH_New(UNKNOWN_REP);
-
-    if (rank == 0) {
-
-      fprintf(stderr,"Reading file in MSTK format...");
-
-      ok = MESH_InitFromFile(mesh,infname,comm);
-
-      fprintf(stderr,"Done\n");
-    }
-  }
-  else {
-    mesh = MESH_New(F1);
-
-    int opts[5]={0,0,0,0,0};
-
-    ok = 0;
-    switch(inmeshformat) {
-    case GMV:
-      if (rank == 0)
-        fprintf(stderr,"Importing mesh from GMV file...");
-      ok = MESH_ImportFromFile(mesh,infname,"gmv",opts,comm);
-      break;
-    case EXODUSII:
-      if (rank == 0)
-        fprintf(stderr,"Importing mesh from ExodusII file...");
-      opts[0] = 0; /* don't partition while importing - do it later */
-      opts[1] = 0;
-      opts[2] = 0; /* no ghost layers */  
-      opts[3] = 0;
-      ok = MESH_ImportFromFile(mesh,infname,"exodusii",opts,comm);
-      break;
-    case NEMESISI:
-      if (rank == 0)
-        fprintf(stderr,"Importing mesh from NemesisI file...");
-      opts[0] = weave ? 1 : 0; 
-      opts[1] = num_ghost_layers; /* no ghost layers */      
-      ok = MESH_ImportFromFile(mesh,infname,"nemesisi",opts,comm);      
-      break;
-    case CGNS:
-      if (rank == 0)
-        fprintf(stderr,"Cannot import mesh from CGNS format. ");
-      break;
-    case VTK:
-      if (rank == 0)
-        fprintf(stderr,"Cannot import mesh from VTK format. ");
-      break;
-    case AVSUCD:
-      if (rank == 0)
-        fprintf(stderr,"Cannot import mesh from AVS format. ");
-      break;
-    case X3D:
-      if (rank == 0)
-        fprintf(stderr,"Importing mesh from X3D format...");
-      ok = MESH_ImportFromFile(mesh,infname,"x3d",opts,comm);
-      break;
-    default:
-      fprintf(stderr,"Cannot import from unrecognized format. ");
-    }
-
-    if (ok) {
-      if (rank == 0)
-        fprintf(stderr,"Done\n");
-    }
-    else {
-      if (rank == 0)
-        fprintf(stderr,"Failed\n");
-      exit(-1);
-    }
-  }
-
-  /* If this is a multi-process run and there is a mesh only on
-   * partition 0, assume that the caller wants to partition the mesh,
-   * otherwise the caller probably wants to weave the mesh */
 
 #ifdef MSTK_HAVE_MPI
   if (numprocs > 1) {
-    int mesh_present = (MESH_Num_Vertices(mesh) > 0) ? 1 : 0;
-    int num_mesh_present = 0;
-    MPI_Allreduce(&mesh_present, &num_mesh_present, 1, MPI_INT, MPI_SUM,
-                  MPI_COMM_WORLD);
+    /* Assume that all parallel files are of the form
+       "base.ext.numprocs.rank" or "base.ext.rank"*/
 
-    if (num_mesh_present == 1)  /* mesh only on one processor */
-      partition = 1;
-    else if (num_mesh_present == numprocs)  /* mesh on all processors */
-      weave = 1;
-    else {
-      fprintf(stderr, "Cannot handle meshes present on subset of processors\n");
-      exit(-1);
+    int filecount = 0;
+    for (int r = 0; r < numprocs; r++) {
+      char tmpfname[256];
+      sprintf(tmpfname,"%s.%-d.%-d",infname,numprocs,r);
+      if ((fp = fopen(tmpfname,"r"))) {
+	filecount++;
+	fclose(fp);
+      }
+    }
+    parallel_file = (filecount == numprocs) ? 1 : 0;
+
+
+    if (serial_file && parallel_file) {
+      /* If both serial and parallel files are present, warn the user
+	 if the defaults don't make sense */
+    
+      if (partition > 0 && weave > 0) { /* conflicting options were specified */
+	MSTK_Report("meshconvert",
+		    "Have serial and parallel input files and BOTH partition and weave options were specified. Pick one: --partition=1|2 will partiton the serial file and --weave=1|y will weave the parallel files",
+		    MSTK_FATAL);
+      } else if ((partition == -1) && (weave == -1)) {
+	if (rank == 0)
+	  MSTK_Report("meshconvert",
+		      "Have serial and parallel input files but NEITHER partition nor weave options were specifeid. Partitioning serial file", MSTK_WARN);
+	partition = 1;
+      }
+      
+    } else if (serial_file || parallel_file) {
+      if (serial_file && partition == -1)  /* serial file and partition option not set */
+	partition = 1;
+      else if (parallel_file && weave == -1)  /* parallel files and weave option not set */
+	weave = 1;
+    } else {
+      MSTK_Report("meshconvert",
+		  "Did not find input file",MSTK_FATAL);
     }
   }
 #endif
 
 
-  if (build_classfn) {
-    if (rank == 0)
-      fprintf(stderr,"Building classification information....");
+  /* now read the mesh */
 
-    ok = MESH_BuildClassfn(mesh,use_geometry);  
-
-    if (ok) {
+  Mesh_ptr mesh;
+  int opts[5]={0,0,0,0,0};
+  
+  if (serial_file) {
+    if (inmeshfmt == EXODUSII) {
+      /* Exodus II read is special - we have a way of doing a faster reading
+	 the serial file and partitioning */
+      
       if (rank == 0)
-        fprintf(stderr,"Done\n");
+	fprintf(stderr,"Importing mesh from ExodusII file...");
+      opts[0] = (partition > 0) ? 1 : 0;
+      opts[1] = (partition > 0) ? partition : 0;
+      opts[2] = 1;
+      opts[3] = partmethod;
+
+      mesh = MESH_New(F1);
+      ok = MESH_ImportFromFile(mesh,infname,"exodusii",opts,comm);
+
+    } else {
+
+      Mesh_ptr serial_mesh = NULL;
+
+
+      if (rank == 0) {
+	ok = 0;
+	switch(inmeshfmt) {
+	case MSTK: {
+	  serial_mesh = MESH_New(UNKNOWN_REP);
+	  fprintf(stderr,"Reading file in MSTK format...");
+	  ok = MESH_InitFromFile(serial_mesh,infname,comm);
+	  fprintf(stderr,"Done\n");
+	  break;
+	}
+	case GMV: {
+	  fprintf(stderr,"Importing mesh from GMV file...");
+	  serial_mesh = MESH_New(F1);
+	  ok = MESH_ImportFromFile(serial_mesh,infname,"gmv",opts,comm);
+	  break;
+	}
+	case X3D: {
+	  fprintf(stderr,"Importing mesh from X3D format...");
+	  serial_mesh = MESH_New(F1);
+	  ok = MESH_ImportFromFile(serial_mesh,infname,"x3d",opts,comm);
+	  break;
+	}
+	case CGNS: case VTK: case AVSUCD: 
+	  if (rank == 0)
+	    MSTK_Report("meshconvert","Cannot import mesh from this format. ",
+			MSTK_FATAL);
+	  break;
+	default:
+	  if (rank == 0)
+	    MSTK_Report("meshconvert","Cannot import from unrecognized format. ",
+			MSTK_FATAL);
+	}
+
+	if (ok)
+	  fprintf(stderr,"Done\n");
+	else {
+	  fprintf(stderr,"Failed\n");
+	  exit(-1);
+	}
+      }
+
+      if (partition > 0) {
+	int ring = 1; /* 1 layer of ghost elements */
+	int with_attr = 1; /* Do allow exchange of attributes */
+	int del_inmesh = 1; /* Delete input mesh after partitioning */
+
+	int dim;
+	if (rank == 0)
+	  dim = MESH_Num_Regions(serial_mesh) ? 3 : 2;
+
+	MPI_Bcast(&dim, 1, MPI_INT, 0, comm);
+	int ok = MSTK_Mesh_Distribute(serial_mesh, &mesh, &dim, ring, with_attr,
+				      partmethod, del_inmesh, comm);
+      } else
+	mesh = serial_mesh;
     }
-    else {
+
+    /* Figure out geometric classification */
+    
+    if (build_classfn) {  /* works correctly only for un-partitioned meshes */
+      if (rank == 0) {
+	fprintf(stderr,"Building classification information....");
+	
+	ok = MESH_BuildClassfn(mesh,use_geometry);  
+
+	if (ok)
+	  fprintf(stderr,"Done\n");
+	else {
+	  fprintf(stderr,"Failed\n");
+	  exit(-1);
+	}
+      }
+    }
+    
+    /* Check that the imported serial mesh topology is ok */
+
+    if (check_topo) {  /* works correctly only for un-partitioned meshes */
+      if (rank == 0) {
+	fprintf(stderr,"Checking mesh topology....");
+
+	ok = MESH_CheckTopo(mesh);
+	
+	if (ok)
+	  fprintf(stderr,"Done\n");
+	else {
+	  fprintf(stderr,"Failed\n");
+	  exit(-1);
+	}
+      }
+    }
+  } else {  /* parallel files */
+
+    char parfilename[256];
+    ok = 0;
+    switch(inmeshfmt) {
+    case MSTK: {
       if (rank == 0)
-        fprintf(stderr,"Failed\n");
+	fprintf(stderr,"Reading file in MSTK format...");
+
+      sprintf(parfilename,"%s.%-d.%-d",infname,numprocs,rank);
+      mesh = MESH_New(UNKNOWN_REP);
+      
+      ok = MESH_InitFromFile(mesh,parfilename,comm);
+      
+      break;
+    }
+    case GMV: {
+      if (rank == 0)
+	fprintf(stderr,"Importing mesh from GMV file...");
+
+      sprintf(parfilename,"%s.%05d",infname,rank+1);
+      mesh = MESH_New(F1);
+      
+      ok = MESH_ImportFromFile(mesh,parfilename,"gmv",opts,comm);
+
+      break;
+    }
+    case NEMESISI: {
+      if (rank == 0)
+	fprintf(stderr,"Reading file in MSTK format...");
+
+      sprintf(parfilename,"%s.%-d.%-d",infname,numprocs,rank);
+      mesh = MESH_New(UNKNOWN_REP);
+      
+      ok = MESH_ImportFromFile(mesh,parfilename,"nenesisi",opts,comm);
+      
+      break;
+    }
+    case CGNS: {
+      if (rank == 0)
+	fprintf(stderr,"Cannot import mesh from CGNS format. ");
+      break;
+    }
+    case VTK: {
+      if (rank == 0)
+	fprintf(stderr,"Cannot import mesh from VTK format. ");
+	break;
+    }
+    case AVSUCD: {
+      if (rank == 0)
+	  fprintf(stderr,"Cannot import mesh from AVS format. ");
+      break;
+    }
+    case X3D: {
+      if (rank == 0)
+	fprintf(stderr,"Importing mesh from X3D format...");
+
+      sprintf(parfilename,"%s.%05d",infname,rank+1);
+      mesh = MESH_New(F1);
+
+      ok = MESH_ImportFromFile(mesh,parfilename,"x3d",opts,comm);
+
+      break;
+    }
+    default:
+      fprintf(stderr,"Cannot import from unrecognized format. ");
+    }
+    
+    if (ok)
+      fprintf(stderr,"Done\n");
+    else {
+      fprintf(stderr,"Failed\n");
       exit(-1);
     }
-  }
-
-  /* Check that the imported mesh is ok - but only if the input mesh is
-   not pre-partitioned. Pre-partitioned meshes will not conform to the 
-   rules that a complete mesh will */
-
-  if (!weave && check_topo)
-    ok = MESH_CheckTopo(mesh);
-
 
 #ifdef MSTK_HAVE_MPI
 
-  if (partition) {
+    if (weave == 1) {
 
-    /* Need to make sure that we have a mesh only on processor 0 */
-    /* For now we cannot repartition                             */
+      int input_type = 0;
+      if (inmeshfmt == NEMESISI)
+	input_type = 1;
+      else if (inmeshfmt == X3D)
+	input_type = 2;
 
-    int prepartitioned = 0, mesh_present = 0;
+      int num_ghost_layers = 1;
+      int dim = MESH_Num_Regions(mesh) ? 3 : 2;
+      MSTK_Weave_DistributedMeshes(mesh, dim, num_ghost_layers, input_type,
+				   comm);
 
-    int dim = 3;
-    if (rank > 0) {
-      if (mesh && MESH_Num_Vertices(mesh) != 0)
-        mesh_present = 1;
     }
     
-    MPI_Reduce(&mesh_present,&prepartitioned,1,MPI_INT,MPI_MAX,0,comm);
-    MPI_Bcast(&prepartitioned,1,MPI_INT,0,comm);
+    if (numprocs > 1 && parallel_check == 1) {
 
-    if (!prepartitioned) {
-    
-      Mesh_ptr mesh0=NULL;
-      if (rank == 0) {
-        fprintf(stderr,"Partitioning mesh into %d parts...",numprocs);
-        
-        if (MESH_Num_Regions(mesh))
-          dim = 3;
-        else if (MESH_Num_Faces(mesh))
-          dim = 2;
-        else {
-          fprintf(stderr,"Partitioning possible only for 2D or 3D meshes\n");
-          exit(-1);
-        }
-        
-        mesh0 = mesh;
-        mesh = NULL;
-      }
+      /* Do a parallel consistency check too */
+      /* Not checking the mesh geometry here */
       
-      int ring = 0; /* No ghost ring of elements */
-      int with_attr = 1; /* Do allow exchange of attributes */
-      int del_inmesh = 1; /* Delete input mesh after partitioning */
-
-      int ok = MSTK_Mesh_Distribute(mesh0, &mesh, &dim, ring, with_attr,
-                                    partmethod, del_inmesh, comm);
+      ok = ok && MESH_Parallel_Check(mesh,comm);
       
-      if (rank == 0) {
-        if (ok)
-          fprintf(stderr,"done\n");
-        else {
-          fprintf(stderr,"failed\n");
-          exit(-1);
-        }
-      }
-
-    }    
-  }
-
-#endif /* MSTK_HAVE_MPI */
-
-
-#ifdef MSTK_HAVE_MPI
-  if (numprocs > 1 && parallel_check == 1) {
-
-    /* Do a parallel consistency check too */
-
-    ok = ok && MESH_Parallel_Check(mesh,comm);
-
-    int allok = 0;
-    MPI_Reduce(&ok,&allok,1,MPI_INT,MPI_MIN,0,comm);
-
-    if (rank == 0 && allok)
-      fprintf(stderr,"Parallel checks passed...\n");
-  }
+      int allok = 0;
+      MPI_Reduce(&ok,&allok,1,MPI_INT,MPI_MIN,0,comm);
+      
+      if (rank == 0 && allok)
+	fprintf(stderr,"Parallel checks passed...\n");
+    }
 #endif
 
-  /* Not checking the mesh geometry here */
+  }  /* if (serial_file) {...} else {...} */
 
+    
 
-  if (outmeshformat == MSTK) {
+    
+
+  if (outmeshfmt == MSTK) {
     if (rank == 0)
       fprintf(stderr,"Writing mesh to MSTK file...");
     MESH_WriteToFile(mesh,outfname,MESH_RepType(mesh),comm);
     fprintf(stderr,"Done\n");
   }
   else {
-    switch(outmeshformat) {
+    switch(outmeshfmt) {
     case GMV:
       if (rank == 0)
         fprintf(stderr,"Exporting mesh to GMV format...");
@@ -469,8 +526,6 @@ int main(int argc, char *argv[]) {
 
   MESH_Delete(mesh);
  
-  // while (1);
-
 #ifdef MSTK_HAVE_MPI
   MPI_Finalize();
 #endif
